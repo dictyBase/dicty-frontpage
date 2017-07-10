44113826ea4a7c20292c38a4ff7d1029e07b0ecc
Alina Gilgur <alinka80@yahoo.com>
2017-07-10 15:11:22 -0500

    Modified styling

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,78 +1,92 @@
-import React from 'react'
-import styled from 'styled-components'
-import twitterLogo from './images/twitterLogo.png'
+// @flow
+import React from 'react';
+import styled from 'styled-components';
+import { Flex, Box } from 'grid-styled';
+import twitterLogo from './images/twitterLogo.png';
 
 const Container = styled.div`
-	text-align: left;
-	padding-left: 10px;
-	padding-right: 10px;
+  text-align: left;
+  padding-left: 10px;
+  padding-right: 10px;
   min-height: 600px;
-  background-color: #F2F2F2;
-`
+  background-color: #f2f2f2;
+`;
 
 const Header = styled.div`
-	color:black;
-	font-size: 20px;
-	padding: 15px 30px 10px 30px;
-	vertical-align:top;
-	text-align: right;
-`
+  color: black;
+  font-size: 20px;
+  padding: 15px 30px 10px 30px;
+  vertical-align: top;
+  text-align: right;
+`;
 
-const Logo = styled.div`
-	float: left;
-`
+const Img = styled.img`width: 25px;`;
 
-const Img = styled.img`
-	width: 25px;
-`
+const NewsBox = styled.li`
+  padding: 0px 25px 10px 35px;
+  font-size: 12px;
+  list-style-type: none;
+  margin-bottom: 5px;
+`;
 
-const newsBox = styled.div`
-	padding: 0px 25px 10px 35px;
-	font-size: 8px;
-`
+const NewsMore = styled.div`
+  color: #0b3861;
+  font-size: 11px;
+  font-style: italic;
+  font-weight: normal;
+  text-align: center;
+  padding-top: 5px;
+  padding-bottom: 15px;
+`;
 
-const newsMore = styled.div`
-	color: #0B3861;
-	font-size: 11px;
-	font-style: italic;
-	font-weight: normal;
-	text-align: center;
-	padding-top: 5px;
-	padding-bottom: 15px;
-`
-/*
-const newsDate = styled.div`
-	color: #0B3861;
-	padding-right: 10px;
-`
-*/
+const NewsDate = styled.div`
+  color: #0b3861;
+  padding-right: 10px;
+  margin-bottom: 5px;
+`;
 
-const News = (props) => {
-			 const text = props.posts.map((post)=>
-					<ul key= {post.id}>
-						<li>
-							{post.date}
-							{post.source}
-							{post.content}
-						</li>
-					</ul>
-				);
-        return (
-					<Container>
-						<Header>
-							<p>DICTY NEWS</p>
-							<Logo>
-								<Img src={ twitterLogo } />
-							</Logo>
-						</Header>
-						<newsBox>
-							{text}
-						</newsBox>
-						<newsMore>
-							<a href="" alt= "more news"> more news </a>
-						</newsMore>
-					</Container>
-        );
-    }
+const NewsContent = styled.div`padding-right: 10px;`;
+
+const NewsSource = styled.div`color: #0b3861;`;
+
+const News = props => {
+  const text = props.posts.map(post =>
+    <ul key={post.id}>
+      <li>
+        <NewsDate>{post.date}</NewsDate>}
+        <NewsSource>{post.source}</NewsSource>
+        <NewsContent>{post.content}</NewsContent>
+      </li>
+    </ul>
+  );
+  return (
+    <Container>
+      <Header>
+        <Flex wrap>
+          <Box px={2} py={1} width={1 / 10}>
+            <a
+              href="https://twitter.com/dictybase"
+              alt="Dicty News at Twitter"
+              target="new"
+            >
+              <Img src={twitterLogo} />
+            </a>
+          </Box>
+          <Box px={2} py={1} width={9 / 10}>
+            DICTY NEWS
+          </Box>
+        </Flex>
+      </Header>
+      <NewsBox>
+        {text}
+      </NewsBox>
+      <NewsMore>
+        <a href="" alt="more news">
+          {' '}more news{' '}
+        </a>
+      </NewsMore>
+    </Container>
+  );
+};
 
 export default News;
