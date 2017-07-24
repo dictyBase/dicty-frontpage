a798b49f60b458d7a6a6c3f2d68d2f20ede5a6a9
Alina Gilgur <alinka80@yahoo.com>
2017-07-24 16:45:26 -0500

    Updated News.test.js

--- src/__tests__/News.test.js
+++ src/__tests__/News.test.js
@@ -1,10 +1,10 @@
 import React from "react"
 import News from "../Components/News"
 import renderer from "react-test-renderer"
-import data from "../././Components/News.json"
+import news from "../././data/news.js"
 
 test("News snapshot test", () => {
-  const component = renderer.create(<News posts={data} />)
+  const component = renderer.create(<News posts={news["news"]} />)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
