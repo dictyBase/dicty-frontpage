ca7fa1361290dfdcb481c1bd0fdbe56782637470
Alina Gilgur <alinka80@yahoo.com>
2017-07-24 15:36:15 -0500

    Create news.js

---
+++ src/data/news.js
@@ -0,0 +1,82 @@
+module.exports = {
+  news: [
+    {
+      date: "2015.08.19",
+      source: "Dicty Stock Center",
+      content:
+        "<strong>Why axenic strains grow by fluid uptake</strong>: We have added the <a href='http://dictybase.org/gene/DDB_G0350652' target='new'> axeB gene </a> to Chromosome 3 at the locus which is largely deleted in axenic strains and thus responsible for their ability to grow axenically. Only an axeB 3' fragment could be generated on the AX4 genome assembly. The full length gene in non-axenic strains such as DdB codes for the conserved neurofibromin 1, a RasGAP, which has been shown to be involved in macropinocytosis and phagocytosis. For more information, see <a href='http://dictybase.org/publication/pubmed/25815683' target='new'>Bloomfield et al</a>.",
+    },
+    {
+      date: "2015.08.12",
+      source: "National Geographics",
+      content:
+        "Dicty in the News: The National Geographics series a <a href='http://phenomena.nationalgeographic.com/2015/08/24/the-bacteria-that-turn-amoebas-into-farmers/?sf12476278=1' target='new'>Not Exactly Rocket Science</a> has published a nice piece about the latest results reporting how Burkholderia turns Dicty amooeba into farmers, <a href='http://www.pnas.org/content/early/2015/08/18/1511878112' target='new'>published</a> by Suzanne di Salvo and colleagues from the Strassmann/Queller lab.",
+    },
+    {
+      date: "2015.08.07",
+      source: "Dicty Stock Center",
+      content:
+        "Starting for DSC orders placed after Monday August 10, 2015 (12:00 AM CDT), Dicty stocks will incur charges. Please see the complete price and payment information on the Order page",
+    },
+    {
+      date: "2015.08.06",
+      source: "Dicty Stock Center",
+      content:
+        "The bug in the DSC ordering system as reported on July 31, has been fixed",
+    },
+    {
+      date: "2015.07.20",
+      source: "Dicty Stock Center",
+      content:
+        "Alert Dicty Stock Center: We currently have problems receiving email notifications of Stock Center orders. If you have ordered in recent months and have not received your order within a week to 10 days, please email Kerry Sheppard and forward your order. If you currently place an order, please also forward your automatic confirmation to Kerry. We are working on the problem and will let you know when fixed. Thank you for your patience and help.",
+    },
+    {
+      date: "2015.07.23",
+      source: "dictyBase",
+      content:
+        "Dicty in the News: The recent discovery of the protein Callipygian in Dictyostelium discoideum provides insights into how round, symmetrical cells become polarized: News Medical with comments from Kristen Swaney and Peter Devreotes.",
+    },
+    {
+      date: "2015.06.12",
+      source: "dictyBase",
+      content:
+        "Dicty in the News: Press releases from three recent research publications highlight the use of Dictyostelium: electrotaxis in Futurity by Zhao and Devreotes; kin recognition in Yahoo News by Ho and Shaulsky; cheating in EurekAlert by Ostrowski, Strassmann and Queller.",
+    },
+    {
+      date: "2015.05.13",
+      source: "dictyBase",
+      content:
+        "Bill Loomis has written a memoir called My Life with Dicty and makes it available on dictyBase.",
+    },
+    {
+      date: "2015.03.11",
+      source: "dicty15",
+      content:
+        "Dicty 2015. Registration is now open for the Annual International Dictyostelium Conference.",
+    },
+    {
+      date: "2015.03.02",
+      source: "dictyBase",
+      content:
+        "Dicty in the News: A recent paper by Falk Hilmann and Thomas Winckler is discussed in DocCheck News in Germany.",
+    },
+    {
+      date: "2014.11.19",
+      source: "dictyBase",
+      content:
+        "The Dicty Stock Center will be closed for Thanksgiving in the week of November 24th - 28th. Please be advised that orders placed from now until November 30 will be shipped in early December. Please do not submit any materials during that week. Thank you for your understanding.",
+    },
+    {
+      date: "2014.11.06",
+      source: "Petra Fey",
+      content:
+        "Dicty in the News: Dictyostelium research details how a cell's gradient sensing and cytoskeletal architecture dictate one mode of chemotactic movement: The Scientist with comments from Chuan-Hsiang Huang",
+    },
+    {
+      date: "2014.10.17",
+      source: "DictyBase",
+      content:
+        "Dicty in the News: Research on the Dictyostelium lpten gene potentially leads to a new way to treat cancer: Drug Discovery and Development with comments from David Soll",
+    },
+  ],
+}
