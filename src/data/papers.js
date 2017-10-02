8a7167e5def5fae3e8f033322f645b66fb9157b5
gowrimanoharir <gowrimanohari.r@gmail.com>
2017-10-02 15:02:07 -0500

    added papers section data, component and styles to display basic Papers section

---
+++ src/data/papers.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    author: "Xie X, Gutierrez A, Trofimov V, Szilágyi I, Soldati T, Bakker E.",
+    title:
+    "<strong><em>Charged Solvatochromic Dyes as Signal Transducers in pH Independent Fluorescent and Colorimetric Ion Selective Nanosensors.</em></strong>",
+    journal: "Anal Chem. 2015 Sep 9.[Epub ahead of print] <a href='https://www.ncbi.nlm.nih.gov/pubmed/26352133' target='new'>Pubmed</a>",
+  },
+  {
+    author: "Schmith A, Spaller T, Gaube F, Fransson Å, Boesler B, Ojha S, Nellen W, Hammann C, Söderbom F, Winckler T.",
+    title:
+    "<strong><em>A host factor supports retrotransposition of the TRE5-A population in Dictyostelium cells by suppressing an Argonaute protein.</em></strong>",
+    journal: "Mob DNA. 2015;6:14. doi: 10.1186/s13100-015-0045-5. <a href='https://www.ncbi.nlm.nih.gov/pubmed/26339297' target='new'>Pubmed</a> ",
+  },
+  {
+    author: "Von Bülow J, Beitz E.",
+    title:
+    "<strong><em>Number and Regulation of Protozoan Aquaporins Reflect Environmental Complexity.</em></strong>",
+    journal: "Biol Bull. 2015 Aug;229(1):38-46. <a href='https://www.ncbi.nlm.nih.gov/pubmed/26338868' target='new'>Pubmed</a> ",
+  },
+  {
+    author: "Santarriaga S, Petersen A, Ndukwe K, Brandt A, Gerges N, Scaglione JB, Scaglione KM.",
+    title:
+    "<strong><em>The Social Amoeba Dictyostelium discoideum is Highly Resistant to Polyglutamine Aggregation.</em></strong>",
+    journal: "J Biol Chem. 2015 Sep 1. doi:pii: jbc.M115.676247. [Epub ahead of print]<a href='https://www.ncbi.nlm.nih.gov/pubmed/26338868' target='new'>Pubmed</a> ",
+  },
+  {
+    author: "Kim MK, Kim JH, Kim JS, Kang SO.",
+    title:
+    "<strong><em>Structure of the 34 kDa F-actin-bundling protein ABP34 from Dictyostelium discoideum.</em></strong>",
+    journal: "Acta Crystallogr D Biol Crystallogr. 2015 Sep 1;71(Pt 9):1835-49. doi: 10.1107/S139900471501264X. Epub 2015 Aug 25. <a href='https://www.ncbi.nlm.nih.gov/pubmed/26338868' target='new'>Pubmed</a> ",
+  },
+  
+]
