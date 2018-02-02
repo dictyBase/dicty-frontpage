cf46ba14e96634e9e4346205c08fd770d3235fb5
Suzy (Sujung) Lee <sujunglee2020@u.northwestern.edu>
2018-02-02 02:12:31 -0600

    Add ability to create unordered bullets

--- src/pages/About.js
+++ src/pages/About.js
@@ -39,6 +39,8 @@ const initialValue = Value.fromJSON({
   }
 })
 
+const DEFAULT_NODE = 'paragraph'
+
 
 export default class About extends Component {
     constructor(props) {
@@ -110,8 +112,21 @@ export default class About extends Component {
       }
     }
 
+    renderNode = (props) => {
+      const { attributes, children, node } = props
+      switch (node.type) {
+        case 'block-quote': return <blockquote {...attributes}>{children}</blockquote>
+        case 'bulleted-list': return <ul {...attributes}>{children}</ul>
+        case 'heading-one': return <h1 {...attributes}>{children}</h1>
+        case 'heading-two': return <h2 {...attributes}>{children}</h2>
+        case 'list-item': return <li {...attributes}>{children}</li>
+        case 'numbered-list': return <ol {...attributes}>{children}</ol>
+      }
+    }
+
     /* HTML Toolbar */
 
+    /* For bold, underline, and italic text */
     hasMark = (type) => {
       const { value } = this.state
       return value.activeMarks.some(mark => mark.type == type)
@@ -134,6 +149,75 @@ export default class About extends Component {
         </Button>
       )
 
+    }
+
+    /* For ordered and unordered bullets */
+
+    hasBlock = (type) => {
+      const { value } = this.state
+      return value.blocks.some(node => node.type == type)
+    }
+
+    onClickBlock = (event, type) => {
+      event.preventDefault()
+      const { value } = this.state
+      const change = value.change()
+      const { document } = value
+
+      // Handle everything but list buttons.
+      if (type != 'bulleted-list' && type != 'numbered-list') {
+        const isActive = this.hasBlock(type)
+        const isList = this.hasBlock('list-item')
+
+        if (isList) {
+          change
+            .setBlock(isActive ? DEFAULT_NODE : type)
+            .unwrapBlock('bulleted-list')
+            .unwrapBlock('numbered-list')
+        }
+
+        else {
+          change
+            .setBlock(isActive ? DEFAULT_NODE : type)
+        }
+      }
+
+      // Handle the extra wrapping required for list buttons.
+      else {
+        const isList = this.hasBlock('list-item')
+        const isType = value.blocks.some((block) => {
+          return !!document.getClosest(block.key, parent => parent.type == type)
+        })
+
+        if (isList && isType) {
+          change
+            .setBlock(DEFAULT_NODE)
+            .unwrapBlock('bulleted-list')
+            .unwrapBlock('numbered-list')
+        } else if (isList) {
+          change
+            .unwrapBlock(type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list')
+            .wrapBlock(type)
+        } else {
+          change
+            .setBlock('list-item')
+            .wrapBlock(type)
+        }
+      }
+
+      this.onChange(change)
+    }
+
+
+    renderBlockButton = (type) => {
+      const isActive = this.hasBlock(type)
+      const onMouseDown = event => this.onClickBlock(event, type)
+
+      return (
+        <Button onMouseDown={onMouseDown} data-active={isActive}>
+          Underline
+        </Button>
+      )
     }
 
     render() {
@@ -159,7 +243,7 @@ export default class About extends Component {
                       {this.renderMarkButton('bold')}
                       {this.renderMarkButton('italic')}
                       {this.renderMarkButton('underline')}
-
+                      {this.renderBlockButton('bulleted-list')}
                     </ToolBar>
 
                     <Editor
@@ -167,6 +251,7 @@ export default class About extends Component {
                       onChange={this.onChange}
                       onKeyDown={this.onKeyDown}
                       renderMark={this.renderMark}
+                      renderNode={this.renderNode}
                     />
                   </Item>
                 </Container>
