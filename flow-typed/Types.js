52a935bc64babf94228ecc3475cec2472e4d7b7b
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-16 14:58:44 -0500

    add flow-typed and config

---
+++ flow-typed/Types.js
@@ -0,0 +1,15 @@
+declare module "draft-js" {
+    declare module.exports: any
+}
+declare module "react-fontawesome" {
+    declare module.exports: any
+}
+declare module "dicty-components-header-footer" {
+    declare module.exports: any
+}
+declare module "dicty-components-navbar" {
+    declare module.exports: any
+}
+declare module "grid-styled" {
+    declare module.exports: any
+}
