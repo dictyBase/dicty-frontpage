dd5a778e0496af053c1d80667881d14c802be54d
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-16 14:13:05 -0500

    update to newest version of header and add new util

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
 // @flow
 import React, { Component } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import { Header, Footer } from "dicty-components-header-footer"
 import { Navbar } from "dicty-components-navbar"
-import styled from "styled-components"
 import { navItems } from "./constants/navbar"
 import items from "./data/footer"
 import Front from "./pages/Front"
 import Learn from "./pages/Learn"
 import About from "./pages/About"
 import Teach from "./pages/Teach"
-
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  row-wrap: wrap;
-  height: 50%;
-  width: 90%;
-  margin: auto;
-`
+import { headerItems, generateLinks } from "./utils/headerItems"
 
 class App extends Component {
-  render() {
-    return (
-        <div>  
-          <Container>
-            <Header downloads="" cite="" info="/about" home="/"/>
-          </Container>
-       
-            <Navbar items={navItems} theme={{}}/>
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Header items={headerItems}>
+                        {items => items.map(generateLinks)}
+                    </Header>
+                </Router>
 
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Front}/>
-              <Route exact path="/explore/learn" component={Learn}/>
-              <Route exact path="/explore/teach" component={Teach}/>              
-              <Route exact path="/about" component={About}/>              
-              <Route component={Front}/>
-            </Switch>
-          </Router>
-          <Footer items={items} />
-        </div>    
-    )
-  }
+                <Navbar items={navItems} theme={{}} />
+
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Front} />
+                        <Route exact path="/explore/learn" component={Learn} />
+                        <Route exact path="/explore/teach" component={Teach} />
+                        <Route exact path="/about" component={About} />
+                        <Route component={Front} />
+                    </Switch>
+                </Router>
+                <Footer items={items} />
+            </div>
+        )
+    }
 }
 
 export default App
-
---
+++ src/utils/headerItems.js
@@ -0,0 +1,69 @@
+// @flow
+import { HeaderLink } from "dicty-components-header-footer"
+import { Link } from "react-router-dom"
+import React from "react"
+import FontAwesome from "react-fontawesome"
+
+const generateLinks = (link: Object, i: string) => {
+    return link.isRouter ? (
+        <Link key={i} to={link.url}>
+            <FontAwesome name={link.icon} />
+            {link.text}
+        </Link>
+    ) : (
+        <HeaderLink key={i} href={link.url}>
+            <FontAwesome name={link.icon} />
+            {link.text}
+        </HeaderLink>
+    )
+}
+
+const headerItems = [
+    {
+        url: "/cite",
+        icon: "plus",
+        text: "Cite Us"
+    },
+    {
+        url: "/downloads",
+        icon: "download",
+        text: "Downloads"
+    },
+    {
+        url: "/about",
+        icon: "info-circle",
+        text: "About dictyBase"
+    },
+    {
+        url: "/login",
+        icon: "sign-in",
+        text: "Login",
+        isRouter: true
+    }
+]
+
+const loggedHeaderItems = [
+    {
+        url: "/cite",
+        icon: "plus",
+        text: "Cite Us"
+    },
+    {
+        url: "/downloads",
+        icon: "download",
+        text: "Downloads"
+    },
+    {
+        url: "/about",
+        icon: "info-circle",
+        text: "About dictyBase"
+    },
+    {
+        url: "/logout",
+        icon: "sign-out",
+        text: "Logout",
+        isRouter: true
+    }
+]
+
+export { headerItems, loggedHeaderItems, generateLinks }
