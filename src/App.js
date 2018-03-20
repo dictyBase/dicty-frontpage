8c65ce256dbde8fd9338fc727a05213068bb9f8d
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-20 09:31:48 -0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 // @flow
-import React, { Component } from "react"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import { Header, Footer } from "dicty-components-header-footer"
-import { Navbar } from "dicty-components-navbar"
-import { navItems } from "./constants/navbar"
-import items from "./data/footer"
-import Front from "./pages/Front"
-import Learn from "./pages/Learn"
-import About from "./pages/About"
-import Teach from "./pages/Teach"
-import { headerItems, generateLinks } from "./utils/headerItems"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Header, Footer } from 'dicty-components-header-footer';
+import { Navbar } from 'dicty-components-navbar';
+import { navItems } from './constants/navbar';
+import items from './data/footer';
+import Front from './pages/Front';
+import Learn from './pages/Learn';
+import About from './pages/About';
+import Teach from './pages/Teach';
+import { headerItems, generateLinks } from './utils/headerItems';
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <Router>
-                    <Header items={headerItems}>
-                        {items => items.map(generateLinks)}
-                    </Header>
-                </Router>
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Header items={headerItems}>{items => items.map(generateLinks)}</Header>
+      </Router>
 
-                <Navbar items={navItems} theme={{}} />
+      <Navbar items={navItems} />
 
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component={Front} />
-                        <Route exact path="/explore/learn" component={Learn} />
-                        <Route exact path="/explore/teach" component={Teach} />
-                        <Route exact path="/about" component={About} />
-                        <Route component={Front} />
-                    </Switch>
-                </Router>
-                <Footer items={items} />
-            </div>
-        )
-    }
-}
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Front} />
+          <Route exact path="/explore/learn" component={Learn} />
+          <Route exact path="/explore/teach" component={Teach} />
+          <Route exact path="/about" component={About} />
+          <Route component={Front} />
+        </Switch>
+      </Router>
+      <Footer items={items} />
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/constants/navbar.js
+++ src/constants/navbar.js
@@ -1,122 +1,116 @@
 export const navItems = [
-    {
-        dropdown: true,
-        title: 'Genomes',
-        items: [
-            {
-                name: 'Genomes Home',
-                href: 'http://www.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Tools',
-        items: [
-            {
-                name: 'Tools Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'New Genome Browser',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Explore',
-        items: [
-            {
-                name: 'Explore Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Dicty Art',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Gallery',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Genome Resources',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Genome Statistics',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Learn About Dicty',
-              href: '/explore/learn'
-            },
-            {
-                name: 'Teaching Protocols',
-                href: '/explore/teach'
-            },
-            {
-              name: 'Useful Links',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Research',
-        items: [
-            {
-                name: 'Research Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Anatomy Ontology',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Codon Bias Table',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Nomenclature Guidelines',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Phenotyping',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Techniques',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Dicty Stock Center',
-        items: [
-            {
-              name: 'Stock Center Home',
-              href: 'http://www.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Community',
-        items: [
-            {
-              name: 'Community Home',
-              href: 'http://www.google.com'
-            },
-            {
-              name: 'Cite Us',
-              href: 'http://www.google.com'
-            },
-            {
-              name: 'Dicty Annual Conferences',
-              href: 'http://www.google.com'
-            }
-        ]
-    }
-]
+  {
+    dropdown: true,
+    title: 'Genomes',
+    items: [
+      {
+        name: 'Genomes Home',
+        href: 'http://www.google.com'
+      }
+    ]
+  },
+  {
+    dropdown: true,
+    title: 'Tools',
+    items: [
+      {
+        name: 'Tools Home',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'New Genome Browser',
+        href: 'http://wwww.google.com'
+      }
+    ]
+  },
+  {
+    dropdown: true,
+    title: 'Explore',
+    items: [
+      {
+        name: 'Explore Home',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Dicty Art',
+        href: 'http://wwww.google.com'
+      },
+      {
+        name: 'Gallery',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Genome Resources',
+        href: 'http://wwww.google.com'
+      },
+      {
+        name: 'Genome Statistics',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Learn About Dicty',
+        href: '/explore/learn'
+      },
+      {
+        name: 'Teaching Protocols',
+        href: '/explore/teach'
+      },
+      {
+        name: 'Useful Links',
+        href: 'http://wwww.google.com'
+      }
+    ]
+  },
+  {
+    dropdown: true,
+    title: 'Research',
+    items: [
+      {
+        name: 'Research Home',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Anatomy Ontology',
+        href: 'http://wwww.google.com'
+      },
+      {
+        name: 'Codon Bias Table',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Nomenclature Guidelines',
+        href: 'http://wwww.google.com'
+      },
+      {
+        name: 'Phenotyping',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Techniques',
+        href: 'http://wwww.google.com'
+      }
+    ]
+  },
+  {
+    title: 'Dicty Stock Center',
+    href: '/stockcenter'
+  },
+  {
+    dropdown: true,
+    title: 'Community',
+    items: [
+      {
+        name: 'Community Home',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Cite Us',
+        href: 'http://www.google.com'
+      },
+      {
+        name: 'Dicty Annual Conferences',
+        href: 'http://www.google.com'
+      }
+    ]
+  }
+];
