e8fbfd09749a867a344d6529006904a2438e647a
wildlifehexagon <crossthebreeze@gmail.com>
2018-03-20 09:48:14 -0500

    update linting rules and fix lint errors

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,6 @@
 // @flow
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import FontAwsome from 'react-fontawesome';
-import CustomEditor from '../Components/aboutpage/CustomEditor';
+import React, { Component } from "react"
+import FontAwesome from "react-fontawesome"
 
 import {
   Banner,
@@ -11,305 +9,315 @@ import {
   Container,
   Item,
   Button,
-  ToolBar
-} from './EditablePageStyles';
+  ToolBar,
+} from "./EditablePageStyles"
 
-import { Editor } from 'slate-react';
-import { Value, withToolBar } from 'slate';
+import { Editor } from "slate-react"
+import { Value } from "slate"
 
 // Create our initial value...
 const initialValue = Value.fromJSON({
   document: {
     nodes: [
       {
-        object: 'block',
-        type: 'paragraph',
+        object: "block",
+        type: "paragraph",
         nodes: [
           {
-            object: 'text',
+            object: "text",
             leaves: [
               {
                 text:
-                  'This beta version of dictyBase was built using AngularJS, with the latest markup (HTML5) and style (CSS3) language versions.\n'
-              }
-            ]
-          }
-        ]
+                  "This beta version of dictyBase was built using AngularJS, with the latest markup (HTML5) and style (CSS3) language versions.\n",
+              },
+            ],
+          },
+        ],
       },
 
       {
-        object: 'block',
-        type: 'paragraph',
+        object: "block",
+        type: "paragraph",
         nodes: [
           {
-            object: 'text',
+            object: "text",
             leaves: [
               {
                 text:
-                  'Bootstrap is the framework used to develop the responsive features.\n'
-              }
-            ]
-          }
-        ]
+                  "Bootstrap is the framework used to develop the responsive features.\n",
+              },
+            ],
+          },
+        ],
       },
 
       {
-        object: 'block',
-        type: 'paragraph',
+        object: "block",
+        type: "paragraph",
         nodes: [
           {
-            object: 'text',
+            object: "text",
             leaves: [
               {
                 text:
-                  'The architecture is hosted entirely on a cloud system. The applications are built and run on docker containers.'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-});
+                  "The architecture is hosted entirely on a cloud system. The applications are built and run on docker containers.",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+})
 
 /* The default mode for text */
-const DEFAULT_NODE = 'paragraph';
+const DEFAULT_NODE = "paragraph"
 
 export default class About extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.state = {
-      value: initialValue // Initial value of editor
-    };
+      value: initialValue, // Initial value of editor
+    }
   }
 
   onChange = ({ value }) => {
-    this.setState({ value }); //
-  };
+    this.setState({ value }) //
+  }
 
   /* Keyboard Hotkeys */
 
   onKeyDown = (event, change) => {
-    console.log('User pressed: ', event.key); // logs keyboard key
-    if (!event.metaKey) return; // if there is no metaKey, quit
+    console.log("User pressed: ", event.key) // logs keyboard key
+    if (!event.metaKey) return // if there is no metaKey, quit
 
     switch (event.key) {
       // if user pressed "b", add "bold" mark to text
-      case 'b': {
-        event.preventDefault();
-        change.toggleMark('bold');
-        return true;
+      case "b": {
+        event.preventDefault()
+        change.toggleMark("bold")
+        return true
       }
 
-      case 'i': {
-        event.preventDefault();
-        change.toggleMark('italic');
-        return true;
+      case "i": {
+        event.preventDefault()
+        change.toggleMark("italic")
+        return true
       }
 
-      case 'u': {
-        event.preventDefault();
-        change.toggleMark('underline');
-        return true;
+      case "u": {
+        event.preventDefault()
+        change.toggleMark("underline")
+        return true
       }
 
       // if the user presses " " then don't change text format
-      case ' ': {
-        event.preventDefault();
-        change.addBlock(' ');
-        return true;
+      case " ": {
+        event.preventDefault()
+        change.addBlock(" ")
+        return true
       }
+
+      default:
+        return
     }
-  };
+  }
 
   renderMark = props => {
     switch (props.mark.type) {
-      case 'bold':
+      case "bold":
         return (
-          console.log('Mark type: ', props.mark.type),
+          console.log("Mark type: ", props.mark.type),
           <strong>{props.children}</strong>
-        );
+        )
 
-      case 'italic':
+      case "italic":
         return (
-          console.log('Mark type: ', props.mark.type), <i>{props.children}</i>
-        );
+          console.log("Mark type: ", props.mark.type), <i>{props.children}</i>
+        )
 
-      case 'underline':
+      case "underline":
         return (
-          console.log('Mark type: ', props.mark.type), <u>{props.children}</u>
-        );
+          console.log("Mark type: ", props.mark.type), <u>{props.children}</u>
+        )
+      default:
+        return
     }
-  };
+  }
 
   renderNode = props => {
-    const { attributes, children, node } = props;
+    const { attributes, children, node } = props
     switch (node.type) {
-      case 'bulleted-list':
-        return <ul {...attributes}>{children}</ul>;
+      case "bulleted-list":
+        return <ul {...attributes}>{children}</ul>
 
-      case 'numbered-list':
-        return <ol {...attributes}>{children}</ol>;
+      case "numbered-list":
+        return <ol {...attributes}>{children}</ol>
 
-      case 'list-item':
-        return <li {...attributes}>{children}</li>;
+      case "list-item":
+        return <li {...attributes}>{children}</li>
 
-      case 'heading-one':
-        return <h1 {...attributes}>{children}</h1>;
+      case "heading-one":
+        return <h1 {...attributes}>{children}</h1>
 
-      case 'heading-two':
-        return <h2 {...attributes}>{children}</h2>;
+      case "heading-two":
+        return <h2 {...attributes}>{children}</h2>
 
-      case 'block-quote':
-        return <blockquote {...attributes}>{children}</blockquote>;
+      case "block-quote":
+        return <blockquote {...attributes}>{children}</blockquote>
+
+      default:
+        return
     }
-  };
+  }
 
   /* HTML Toolbar */
 
   /* For bold, underline, and italic text */
   hasMark = type => {
-    const { value } = this.state;
-    return value.activeMarks.some(mark => mark.type == type);
-  };
+    const { value } = this.state
+    return value.activeMarks.some(mark => mark.type === type)
+  }
 
   onClickMark = (event, type) => {
-    event.preventDefault();
-    const { value } = this.state;
-    const change = value.change().toggleMark(type);
-    this.onChange(change);
-  };
+    event.preventDefault()
+    const { value } = this.state
+    const change = value.change().toggleMark(type)
+    this.onChange(change)
+  }
 
   renderMarkButton = type => {
-    const isActive = this.hasMark(type);
-    const onMouseDown = event => this.onClickMark(event, type);
+    const isActive = this.hasMark(type)
+    const onMouseDown = event => this.onClickMark(event, type)
 
     return (
       <Button onMouseDown={onMouseDown} data-active={isActive}>
-        <FontAwsome name={type} />
+        <FontAwesome name={type} />
       </Button>
-    );
-  };
+    )
+  }
 
   /* For ordered and unordered bullets */
 
   hasBlock = type => {
-    const { value } = this.state;
-    return value.blocks.some(node => node.type == type);
-  };
+    const { value } = this.state
+    return value.blocks.some(node => node.type === type)
+  }
 
   onClickBlock = (event, type) => {
-    event.preventDefault();
-    const { value } = this.state;
-    const change = value.change();
-    const { document } = value;
+    event.preventDefault()
+    const { value } = this.state
+    const change = value.change()
+    const { document } = value
 
     // Handle anything that aren't lists
-    if (type != 'bulleted-list' && type != 'numbered-list') {
-      const isActive = this.hasBlock(type);
-      const isList = this.hasBlock('list-item');
+    if (type !== "bulleted-list" && type !== "numbered-list") {
+      const isActive = this.hasBlock(type)
+      const isList = this.hasBlock("list-item")
 
       if (isList) {
         change
           .setBlock(isActive ? DEFAULT_NODE : type)
-          .unwrapBlock('bulleted-list')
-          .unwrapBlock('numbered-list');
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list")
       } else {
-        change.setBlock(isActive ? DEFAULT_NODE : type);
+        change.setBlock(isActive ? DEFAULT_NODE : type)
       }
     } else {
       // Handle the extra wrapping required for list buttons.
-      const isList = this.hasBlock('list-item');
+      const isList = this.hasBlock("list-item")
       const isType = value.blocks.some(block => {
-        return !!document.getClosest(block.key, parent => parent.type == type);
-      });
+        return !!document.getClosest(block.key, parent => parent.type === type)
+      })
 
       if (isList && isType) {
         change
           .setBlock(DEFAULT_NODE)
-          .unwrapBlock('bulleted-list')
-          .unwrapBlock('numbered-list');
+          .unwrapBlock("bulleted-list")
+          .unwrapBlock("numbered-list")
       } else if (isList) {
         change
           .unwrapBlock(
-            type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list'
+            type === "bulleted-list" ? "numbered-list" : "bulleted-list",
           )
-          .wrapBlock(type);
+          .wrapBlock(type)
       } else {
-        change.setBlock('list-item').wrapBlock(type);
+        change.setBlock("list-item").wrapBlock(type)
       }
     }
 
-    this.onChange(change);
-  };
+    this.onChange(change)
+  }
 
   renderBlockButton = type => {
-    const isActive = this.hasBlock(type);
-    const onMouseDown = event => this.onClickBlock(event, type);
+    const isActive = this.hasBlock(type)
+    const onMouseDown = event => this.onClickBlock(event, type)
 
     switch (type) {
-      case 'bulleted-list':
+      case "bulleted-list":
         return (
           <Button onMouseDown={onMouseDown} data-active={isActive}>
-            <FontAwsome name="list-ul" />
+            <FontAwesome name="list-ul" />
           </Button>
-        );
-      case 'numbered-list':
+        )
+      case "numbered-list":
         return (
           <Button onMouseDown={onMouseDown} data-active={isActive}>
-            <FontAwsome name="list-ol" />
+            <FontAwesome name="list-ol" />
           </Button>
-        );
-      case 'heading-one':
+        )
+      case "heading-one":
         return (
           <Button onMouseDown={onMouseDown} data-active={isActive}>
-            <FontAwsome name="header" />
+            <FontAwesome name="header" />
           </Button>
-        );
+        )
       // case "heading-two":
       //   return (
       //     <Button onMouseDown={onMouseDown} data-active={isActive}>
-      //       <FontAwsome name="heart" />
+      //       <FontAwesome name="heart" />
       //     </Button>
       //   );
-      case 'block-quote':
+      case "block-quote":
         return (
           <Button onMouseDown={onMouseDown} data-active={isActive}>
-            <FontAwsome name="indent" />
+            <FontAwesome name="indent" />
           </Button>
-        );
+        )
+      default:
+        return
     }
-  };
+  }
 
   render() {
-    const itemStyle = {
-      paddingTop: '20px',
-      marginTop: '10px'
-    };
+    // const itemStyle = {
+    //     paddingTop: "20px",
+    //     marginTop: "10px"
+    // }
 
     return (
       <div>
         <Banner>
           <Header>About Us</Header>
           <Hdrtxt>
-            We{'  '}
-            <FontAwsome name="heart fa-2x" />
-            {'  '}dictyBase
+            We{"  "}
+            <FontAwesome name="heart fa-2x" />
+            {"  "}dictyBase
           </Hdrtxt>
         </Banner>
 
         <Container>
           <Item>
             <ToolBar>
-              {this.renderMarkButton('bold')}
-              {this.renderMarkButton('italic')}
-              {this.renderMarkButton('underline')}
-              {this.renderBlockButton('bulleted-list')}
-              {this.renderBlockButton('numbered-list')}
-              {this.renderBlockButton('heading-one')}
+              {this.renderMarkButton("bold")}
+              {this.renderMarkButton("italic")}
+              {this.renderMarkButton("underline")}
+              {this.renderBlockButton("bulleted-list")}
+              {this.renderBlockButton("numbered-list")}
+              {this.renderBlockButton("heading-one")}
               {/* {this.renderBlockButton("heading-two")} */}
-              {this.renderBlockButton('block-quote')}
+              {this.renderBlockButton("block-quote")}
             </ToolBar>
 
             <Editor
@@ -322,6 +330,6 @@ export default class About extends Component {
           </Item>
         </Container>
       </div>
-    );
+    )
   }
 }
