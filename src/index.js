916cb2dc17b52c41c510d3764f7577c8ea29a243
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-01 15:41:09 -0500

    add store hydration

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import { ConnectedRouter } from "react-router-redux"
+import { hydrateStore } from "dicty-components-redux"
 import configureStore, { history } from "./store/configureStore"
 import "./index.css"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 
-const store = configureStore()
+// load state from localStorage(if any) to set the initial state for the store
+const initialState = hydrateStore({ key: "auth", namespace: "auth" })
+
+const store = configureStore(initialState)
 
 ReactDOM.render(
   <Provider store={store}>
