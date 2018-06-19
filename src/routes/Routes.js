2694054779c1eb3ef58974d93e8d9b678b4d8e78
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-19 11:22:58 -0500

    remove editnews route, will just use inline editor

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -8,7 +8,6 @@ import Learn from "components/pages/Learn"
 import About from "components/pages/About/About"
 import Teach from "components/pages/Teach"
 import AddNews from "components/pages/News/AddNews"
-import EditNews from "components/pages/News/EditNews"
 import Login from "components/authentication/Login"
 import OauthCallback from "components/authentication/OauthCallback"
 import AuthLoader from "components/authentication/AuthLoader"
@@ -22,7 +21,6 @@ const Routes = () => {
       <Route exact path="/explore/teach" component={Teach} />
       <Route exact path="/about" component={About} />
       <PrivateRoute exact path="/addnews" component={AddNews} />
-      <PrivateRoute exact path="/editnews" component={EditNews} />
       <LoginRoute exact path="/login" component={Login} />
       <Route exact path="/:provider/callback" component={OauthCallback} />
       <Route exact path="/load/auth" component={AuthLoader} />
