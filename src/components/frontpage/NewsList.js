e5bf197649cc203a3f1feee820261ec1f19fddf4
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-20 12:01:19 -0500

    add link to add news if user has right permissions

--- src/components/frontpage/NewsList.js
+++ src/components/frontpage/NewsList.js
@@ -1,6 +1,13 @@
 // @flow
 import React, { Fragment } from "react"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import Authorization from "components/authentication/Authorization"
 import NewsItem from "./NewsItem"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 /** This component has fake data right now, but it will be set up to map over fetched data by date and display the three most recent news items. */
 
@@ -10,6 +17,20 @@ const NewsList = () => {
       <NewsItem slug="frontpagenews-2015-08-19" />
       <NewsItem slug="frontpagenews-2015-08-12" />
       <NewsItem slug="frontpagenews-2015-08-07" />
+      <Authorization
+        render={({ canAddNews, verifiedToken }) => {
+          return (
+            <div>
+              {canAddNews &&
+                verifiedToken && (
+                  <center>
+                    <StyledLink to="/addnews">Add News Item</StyledLink>
+                  </center>
+                )}
+            </div>
+          )
+        }}
+      />
     </Fragment>
   )
 }
