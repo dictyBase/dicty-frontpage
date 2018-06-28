01ce54dfa652940cff49f1a365c8c3431f1dda4e
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-28 12:28:23 -0500

    add deserializer tool

---
+++ src/components/editor/tools/deserializer.js
@@ -0,0 +1,85 @@
+import Html from "slate-html-serializer"
+import { Editor, getEventTransfer } from "slate-react"
+import { Value } from "slate"
+
+const BLOCK_TAGS = {
+  p: "paragraph",
+  li: "list-item",
+  ul: "bulleted-list",
+  ol: "numbered-list",
+  blockquote: "quote",
+  pre: "code",
+  h1: "heading-one",
+  h2: "heading-two",
+  h3: "heading-three",
+  h4: "heading-four",
+  h5: "heading-five",
+  h6: "heading-six",
+  figure: "figure",
+  figcaption: "figcaption",
+  hr: "divider",
+  table: "table",
+  th: "table-head",
+  tr: "table-row",
+  td: "table-cell",
+  center: "center",
+}
+
+const MARK_TAGS = {
+  strong: "bold",
+  b: "bold",
+  em: "italic",
+  i: "italic",
+  u: "underline",
+  s: "strikethrough",
+  code: "code",
+}
+
+const rules = [
+  {
+    deserialize(el, next) {
+      const tagName = el.tagName.toLowerCase()
+      if (tagName === "img") {
+        // special case for images, to grab their src
+        return {
+          object: "block",
+          type: "image",
+          isVoid: true,
+          nodes: next(el.childNodes),
+          data: {
+            src: el.getAttribute("src"),
+          },
+        }
+      } else if (tagName === "a") {
+        // special case for links, to grab their href
+        return {
+          object: "inline",
+          type: "link",
+          nodes: next(el.childNodes),
+          data: {
+            href: el.getAttribute("href"),
+          },
+        }
+      } else if (BLOCK_TAGS[tagName]) {
+        // rule to handle blocks
+        return {
+          object: "block",
+          type: BLOCK_TAGS[tagName],
+          data: {
+            className: el.getAttribute("class"),
+          },
+          nodes: next(el.childNodes),
+        }
+      } else if (MARK_TAGS[tagName]) {
+        // rule to handle marks
+        return {
+          object: "mark",
+          type: MARK_TAGS[tagName],
+          nodes: next(el.childNodes),
+        }
+      }
+    },
+  },
+]
+
+export const serializer = new Html({ rules: rules })
