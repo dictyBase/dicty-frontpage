5e25d56db1a53f340cee4b6f277c0682e1bdcce1
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-03 11:46:10 -0500

    add content to help modal

--- src/components/editor/docs/HelpModal.js
+++ src/components/editor/docs/HelpModal.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react"
+import React from "react"
+import { Flex, Box } from "rebass"
 import { withStyles } from "@material-ui/core/styles"
 import Modal from "@material-ui/core/Modal"
 import { SaveButton } from "styles/EditablePageStyles"
 
 function getModalStyle() {
-  const top = 25
-
   return {
-    top: `${top}%`,
+    top: "15%",
     margin: "auto",
   }
 }
@@ -20,38 +19,73 @@ const styles = theme => ({
     boxShadow: theme.shadows[5],
     padding: theme.spacing.unit * 4,
     borderRadius: "5px",
+    backgroundColor: "#faf9f9",
   },
 })
 
-class HelpMuiModal extends Component {
-  render() {
-    return (
-      <Modal
-        open={this.props.helpModalOpen}
-        style={{ justifyContent: "center" }}>
-        <div style={getModalStyle()} className={this.props.classes.paper}>
-          <h1>Editor Help</h1>
-          <p>
-            <strong>Keyboard Shortcuts:</strong>
-          </p>
-          <p>
-            <strong>Bold</strong> - CTRL + B<br />
-            <em>Italic</em> - CTRL + Z<br />
-            <strike>Strikethrough</strike> - CTRL + D<br />
-            <u>Underline</u> - CTRL + U
-          </p>
-          <SaveButton
-            style={{
-              width: "50%",
-              color: "#fff",
-            }}
-            onClick={this.props.handleClose}>
-            Close
-          </SaveButton>
-        </div>
-      </Modal>
-    )
-  }
+const HelpMuiModal = props => {
+  return (
+    <Modal open={props.helpModalOpen} style={{ justifyContent: "center" }}>
+      <div style={getModalStyle()} className={props.classes.paper}>
+        <Flex justify="center" direction="column">
+          <Box w={"100%"}>
+            <center>
+              <h1>Editor Help</h1>
+            </center>
+          </Box>
+          <Box w={"100%"}>
+            <p>
+              <h3>Keyboard Shortcuts:</h3>
+            </p>
+            <p>
+              <Flex wrap>
+                <Box w="50%">
+                  <strong>Bold</strong>
+                </Box>
+                <Box w="50%">CTRL + B</Box>
+                <Box w="50%">
+                  <em>Italic</em>
+                </Box>
+                <Box w="50%">CTRL + I</Box>
+                <Box w="50%">
+                  <strike>Strikethrough</strike>
+                </Box>
+                <Box w="50%">CTRL + D</Box>
+                <Box w="50%">
+                  <u>Underline</u>
+                </Box>
+                <Box w="50%">CTRL + U</Box>
+              </Flex>
+            </p>
+            <p>
+              <h3>Pasting Content</h3>
+            </p>
+            <p>
+              Most content from HTML pages can be copied and pasted directly
+              into the editor like this:
+            </p>
+            <p>(insert gif here)</p>
+            <p>
+              Links can easily be added by highlighting text and pasting a URL.
+            </p>
+            <p>Images can be added by copying and pasting an image URL.</p>
+            <p>
+              Videos can be added by copying and pasting the video URL (either
+              YouTube or Vimeo).
+            </p>
+            <SaveButton
+              style={{
+                width: "50%",
+                color: "#fff",
+              }}
+              onClick={props.handleClose}>
+              Close
+            </SaveButton>
+          </Box>
+        </Flex>
+      </div>
+    </Modal>
+  )
 }
 
 const HelpModal = withStyles(styles)(HelpMuiModal)
