927b049b69419fd0167aff64b3b9e7f316eaccf0
wildlifehexagon <crossthebreeze@gmail.com>
2018-07-23 15:13:05 -0500

    add react-ga and tracking id

--- src/index.js
+++ src/index.js
@@ -15,6 +15,24 @@ const initialState = hydrateStore({ key: "auth", namespace: "auth" })
 
 const store = configureStore(initialState)
 
+const setGoogleAnalytics = async (location, action) => {
+  try {
+    const module = await import("react-ga")
+    let ReactGA = module.default
+    ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID)
+    ReactGA.set({ page: window.location.pathname })
+    ReactGA.pageview(window.location.pathname)
+  } catch (e) {
+    console.error("could not load react-ga module", JSON.stringify(e))
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  history.listen((location, action) => {
+    setGoogleAnalytics(location, action)
+  })
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
