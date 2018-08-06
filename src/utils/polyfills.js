3aa36d045f98cd6450b074f48665784c21c39b79
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-06 15:18:16 -0500

    update polyfills to use libraries

--- src/utils/polyfills.js
+++ src/utils/polyfills.js
@@ -2,33 +2,5 @@ import "core-js/es6/string"
 import "core-js/es6/array"
 import "core-js/es7/array"
 import "babel-polyfill"
-;(function(arr) {
-  arr.forEach(function(item) {
-    if (item.hasOwnProperty("remove")) {
-      return
-    }
-    Object.defineProperty(item, "remove", {
-      configurable: true,
-      enumerable: true,
-      writable: true,
-      value: function remove() {
-        this.parentNode.removeChild(this)
-      },
-    })
-  })
-})([Element.prototype, CharacterData.prototype, DocumentType.prototype])
-
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    "use strict"
-    if (typeof start !== "number") {
-      start = 0
-    }
-
-    if (start + search.length > this.length) {
-      return false
-    } else {
-      return this.indexOf(search, start) !== -1
-    }
-  }
-}
+import "string.prototype.includes"
+import "element-remove"
