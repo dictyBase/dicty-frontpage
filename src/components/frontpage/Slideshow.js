23fcead03f56b1441829a434ec0b45106d3a79b8
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-15 11:46:53 -0500

    fix image display on smaller devices #60

--- src/components/frontpage/Slideshow.js
+++ src/components/frontpage/Slideshow.js
@@ -6,6 +6,7 @@ import {
   SlideshowContainer,
   ImageDiv,
   ImageSizer,
+  Legend,
 } from "styles/SlideshowStyles"
 import lifeCycle from "images/frontCarousel/dicty-life-cycle.jpg"
 import slug from "images/frontCarousel/dicty-slug.jpg"
@@ -27,30 +28,30 @@ const Slideshow = () => {
         infiniteLoop>
         <ImageDiv>
           <ImageSizer src={lifeCycle} alt="dicty life cycle" />
-          <p className="legend">
+          <Legend>
             <strong>Dicty Life Cycle</strong> Courtesy of M.J. Grimson & R.L.
             Blanton, Biological Sciences Electron Microscopy Laboratory, Texas
             Tech University
-          </p>
+          </Legend>
         </ImageDiv>
         <ImageDiv>
           <ImageSizer src={slug} alt="D. discoideum slug" />
-          <p className="legend">
+          <Legend>
             <strong>D. discoideum slug</strong> Courtesy Dirk Dormann, MRC
             London Institute of Medical Sciences
-          </p>
+          </Legend>
         </ImageDiv>
-        <div style={{ height: "440px" }}>
+        <ImageDiv>
           <ImageSizer src={tubulin} alt="ggtA-mutant-DG1109" />
-          <p className="legend">
+          <Legend>
             <strong>
               Triple stained and fixed Dictyostelium cells: apha-tubulin
               (green), centrosome (red, appears yellow due to colocalization
               with tubulin), nuclei (blue)
             </strong>{" "}
             Courtesy of Ralph Gräf, Potsdam University
-          </p>
-        </div>
+          </Legend>
+        </ImageDiv>
       </Carousel>
     </SlideshowContainer>
   )
--- src/styles/SlideshowStyles.js
+++ src/styles/SlideshowStyles.js
@@ -9,10 +9,47 @@ export const SlideshowContainer = styled.div`
 
 export const ImageDiv = styled.div`
   height: 440px;
+
+  @media (max-width: 768px) {
+    height: 250px;
+  }
 `
 
 export const ImageSizer = styled.img`
   height: 100%;
   width: 100%;
   max-height: 100%;
+`
+
+export const Legend = styled.p`
+  -webkit-transition: all 0.5s ease-in-out;
+  -moz-transition: all 0.5s ease-in-out;
+  -ms-transition: all 0.5s ease-in-out;
+  -o-transition: all 0.5s ease-in-out;
+  transition: all 0.5s ease-in-out;
+  position: absolute;
+  bottom: 40px;
+  left: 50%;
+  margin-left: -45%;
+  width: 90%;
+  border-radius: 10px;
+  background: #000;
+  color: #fff;
+  padding: 10px;
+  font-size: 12px;
+  text-align: center;
+  opacity: 0.25;
+  -webkit-transition: opacity 0.35s ease-in-out;
+  -moz-transition: opacity 0.35s ease-in-out;
+  -ms-transition: opacity 0.35s ease-in-out;
+  -o-transition: opacity 0.35s ease-in-out;
+  transition: opacity 0.35s ease-in-out;
+
+  &:hover {
+    opacity: 1;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  }
 `
