816e492fb3b27dce8009662175c605f574a13327
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-21 10:53:38 -0500

    update flow-typed definitions

--- flow-typed/Types.js
+++ flow-typed/Types.js
@@ -1,7 +1,15 @@
 declare module "font-awesome/css/font-awesome.min.css" {
   declare module.exports: any
 }
+declare module "react-responsive-carousel/lib/styles/carousel.min.css" {
+  declare module.exports: any
+}
 
 class process {
-  static env: { REACT_APP_API_SERVER: string }
+  static env: {
+    REACT_APP_API_SERVER: string,
+    REACT_APP_BASENAME: string,
+    REACT_APP_NAVBAR_JSON: string,
+    REACT_APP_FOOTER_JSON: string
+  }
 }
