9d3de00090327fc73b7a76b3fae9de9c87c1a2f9
wildlifehexagon <crossthebreeze@gmail.com>
2018-08-31 14:38:40 -0500

    refactor: set up second fetch request to get individual json data in one shot
    this separates the downloads state by taxon ID for each tab's respective data

--- src/actions/downloads.js
+++ src/actions/downloads.js
@@ -3,6 +3,9 @@ import {
   FETCH_DOWNLOAD_TABS_REQUEST,
   FETCH_DOWNLOAD_TABS_SUCCESS,
   FETCH_DOWNLOAD_TABS_FAILURE,
+  FETCH_DOWNLOAD_CONTENT_REQUEST,
+  FETCH_DOWNLOAD_CONTENT_SUCCESS,
+  FETCH_DOWNLOAD_CONTENT_FAILURE,
   CHANGE_TAB,
 } from "constants/types"
 
@@ -25,6 +28,29 @@ const fetchDownloadTabsSuccess = (json: Object) => ({
 
 const fetchDownloadTabsFailure = error => ({
   type: FETCH_DOWNLOAD_TABS_FAILURE,
+  payload: {
+    error,
+  },
+})
+
+const fetchDownloadContentRequest = () => ({
+  type: FETCH_DOWNLOAD_CONTENT_REQUEST,
+  payload: {
+    isFetching: true,
+  },
+})
+
+const fetchDownloadContentSuccess = (id: string, json: Object) => ({
+  type: FETCH_DOWNLOAD_CONTENT_SUCCESS,
+  payload: {
+    isFetching: false,
+    id,
+    data: json,
+  },
+})
+
+const fetchDownloadContentFailure = error => ({
+  type: FETCH_DOWNLOAD_CONTENT_FAILURE,
   payload: {
     error,
   },
@@ -59,6 +85,7 @@ export const fetchDownloadTabs = () => async (dispatch: Function) => {
     const json = await res.json()
 
     if (res.ok) {
+      dispatch(getDownloadLinks(json.data))
       const normalized = normalizeData(json)
       return dispatch(fetchDownloadTabsSuccess(normalized))
     }
@@ -66,4 +93,35 @@ export const fetchDownloadTabs = () => async (dispatch: Function) => {
   } catch (error) {
     return dispatch(fetchDownloadTabsFailure(error.toString()))
   }
+}
+
+export const fetchDownloadContent = (id: string, url: string) => async (
+  dispatch: Function,
+) => {
+  try {
+    dispatch(fetchDownloadContentRequest())
+    const res = await fetch(url)
+    const json = await res.json()
+
+    if (res.ok) {
+      return dispatch(fetchDownloadContentSuccess(id, json))
+    }
+    dispatch(fetchDownloadContentFailure(res.statusText))
+  } catch (error) {
+    return dispatch(fetchDownloadContentFailure(error.toString()))
+  }
+}
+
+// get the download relationship links to fetch individual data
+const getDownloadLinks = data => dispatch => {
+  const links = data.map(item => {
+    const url = item.relationships.downloads.links.related
+    const id = item.id
+    return [id, url]
+  })
+  const fetchContent = links.forEach(item => {
+    dispatch(fetchDownloadContent(item[0], item[1]))
+  })
+
+  return fetchContent
 }
