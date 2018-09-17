708d68e28125bb84e76177e5e387355f583a2b7e
wildlifehexagon <crossthebreeze@gmail.com>
2018-09-17 12:35:32 -0500

    feat(HelpModal): add ClickAwayListener so user can click anywhere outside modal to close

--- src/components/editor/HelpModal.js
+++ src/components/editor/HelpModal.js
@@ -2,6 +2,7 @@ import React from "react"
 import { withStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import Modal from "@material-ui/core/Modal"
+import ClickAwayListener from "@material-ui/core/ClickAwayListener"
 import { SaveButton } from "styles/EditablePageStyles"
 
 function getModalStyle() {
@@ -36,45 +37,49 @@ const styles = theme => ({
 
 const HelpMuiModal = props => {
   const { showHelpModal, classes, handleClose } = props
+  console.log(handleClose)
   return (
     <Modal className={classes.modal} open={showHelpModal}>
-      <div style={getModalStyle()} className={classes.paper}>
-        <Grid container justify="center" direction="column">
-          <Grid item xs={12}>
-            <center>
-              <h1>Editor Help</h1>
-            </center>
+      <ClickAwayListener onClickAway={handleClose}>
+        <div style={getModalStyle()} className={classes.paper}>
+          <Grid container justify="center" direction="column">
+            <Grid item xs={12}>
+              <center>
+                <h1>Editor Help</h1>
+              </center>
+            </Grid>
+            <Grid item xs={12}>
+              <h3>Pasting Content</h3>
+              <p>
+                Some content from HTML pages can be copied and pasted directly
+                into the editor. Just go to any page, highlight what you want to
+                copy then paste it into the editor.
+              </p>
+              <p>
+                Links can easily be added by highlighting text and pasting a
+                URL.
+              </p>
+              <p>
+                Images can be added by simply copying and pasting an image URL.
+              </p>
+              <p>
+                Videos can be added by copying and pasting the video URL (either
+                YouTube or Vimeo). This will automatically embed the video.
+              </p>
+              <h3>Keyboard Shortcuts</h3>
+              <p>
+                Some features have keyboard shortcuts. These can be found by
+                hovering over a button in the toolbar. These tooltips will show
+                any applicable shortcuts. For example, <strong>bold</strong> is
+                CTRL + B.
+              </p>
+              <SaveButton className={classes.save} onClick={handleClose}>
+                Close
+              </SaveButton>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <h3>Pasting Content</h3>
-            <p>
-              Most content from HTML pages can be copied and pasted directly
-              into the editor. Just go to any page, highlight what you want to
-              copy then paste it into the editor.
-            </p>
-            <p>
-              Links can easily be added by highlighting text and pasting a URL.
-            </p>
-            <p>
-              Images can be added by simply copying and pasting an image URL.
-            </p>
-            <p>
-              Videos can be added by copying and pasting the video URL (either
-              YouTube or Vimeo). This will automatically embed the video.
-            </p>
-            <h3>Keyboard Shortcuts</h3>
-            <p>
-              Some features have keyboard shortcuts. These can be found by
-              hovering over a button in the toolbar. These tooltips will show
-              any applicable shortcuts. For example, <strong>bold</strong> is
-              CTRL + B.
-            </p>
-            <SaveButton className={classes.save} onClick={handleClose}>
-              Close
-            </SaveButton>
-          </Grid>
-        </Grid>
-      </div>
+        </div>
+      </ClickAwayListener>
     </Modal>
   )
 }
