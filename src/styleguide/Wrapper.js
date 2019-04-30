fad87cde57c06266c245ebc6796fceb5445187ee
wildlifehexagon <crossthebreeze@gmail.com>
2019-04-30 11:46:08 -0500

    chore: generate styleguide build, update react-scripts and styleguidist
    fix #109

---
+++ src/styleguide/Wrapper.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
+import rootReducer from "reducers"
+
+let store = createStore(rootReducer)
+export default class Wrapper extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>{this.props.children}</BrowserRouter>
+      </Provider>
+    )
+  }
+}
