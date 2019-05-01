d5ef5c04bbc6e9a5dc21f53a863054b1f1a2f678
wildlifehexagon <crossthebreeze@gmail.com>
2019-05-01 10:18:40 -0500

    feat: add more font options to the editor toolbar
    fix #103

--- src/styles/Body.js
+++ src/styles/Body.js
@@ -3,6 +3,9 @@ import "typeface-roboto"
 import "typeface-roboto-condensed"
 import "typeface-roboto-mono"
 import "typeface-roboto-slab"
+import "typeface-lato"
+import "typeface-merriweather"
+import "typeface-montserrat"
 
 injectGlobal([
   `
