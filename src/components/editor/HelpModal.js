80f4e1e837bf5ec4baa43611c0bf76cad06e4880
wildlifehexagon <crossthebreeze@gmail.com>
2019-05-31 15:49:43 -0500

    refactor: convert HelpModal to Dialog component, update its notes

--- src/components/editor/HelpModal.js
+++ src/components/editor/HelpModal.js
@@ -2,26 +2,13 @@
 import React from "react"
 import { withStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
-import Modal from "@material-ui/core/Modal"
+import Dialog from "@material-ui/core/Dialog"
+import DialogActions from "@material-ui/core/DialogActions"
 import DialogContent from "@material-ui/core/DialogContent"
-import ClickAwayListener from "@material-ui/core/ClickAwayListener"
+import DialogTitle from "@material-ui/core/DialogTitle"
 import { SaveButton } from "styles/EditablePageStyles"
 
 const styles = theme => ({
-  paper: {
-    position: "absolute",
-    width: theme.spacing.unit * 75,
-    backgroundColor: "#faf9f9",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 2,
-    borderRadius: "5px",
-    top: "5%",
-    margin: "auto",
-  },
-  modal: {
-    display: "flex",
-    justifyContent: "center",
-  },
   save: {
     width: "50%",
     color: "#fff",
@@ -48,76 +35,79 @@ const HelpMuiModal = (props: Props) => {
   const { showHelpModal, classes, handleClose } = props
 
   return (
-    <Modal className={classes.modal} open={showHelpModal}>
-      <DialogContent className={classes.paper}>
-        <ClickAwayListener onClickAway={handleClose}>
-          <Grid container justify="center" direction="column">
-            <Grid item xs={12}>
-              <center>
-                <h1>Editor Help</h1>
-              </center>
-            </Grid>
-            <Grid item xs={12}>
-              <h3>Pasting Content</h3>
-              <p>
-                Some content from HTML pages can be copied and pasted directly
-                into the editor. Just go to any page, highlight what you want to
-                copy then paste it into the editor.
-              </p>
-              <p>
-                Until we develop an image uploading backend, please put any
-                images you want to use in{" "}
+    <Dialog open={showHelpModal} onClose={handleClose}>
+      <DialogTitle id="help-dialog-title">Editor Help</DialogTitle>
+      <DialogContent>
+        <Grid container justify="center" direction="column">
+          <Grid item xs={12}>
+            <h3>Pasting Content</h3>
+            <p>
+              Some content from HTML pages can be copied and pasted directly
+              into the editor. Just go to any page, highlight what you want to
+              copy then paste it into the editor.
+            </p>
+            <p>
+              Upload any images{" "}
+              <a
+                className={classes.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/dictyBase/migration-data/issues/9">
+                here
+              </a>
+              .
+            </p>
+            <p>
+              Upload any PDFs or DOCX files{" "}
+              <a
+                className={classes.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://github.com/dictyBase/migration-data/issues/10">
+                here
+              </a>
+              .
+            </p>
+            <p>
+              <strong>Known bugs:</strong> Lists sometimes have a mind of their
+              own, but there are some workarounds for formatting. You can use
+              SHIFT + ENTER to add a space between list items.
+            </p>
+            <h3>Keyboard Shortcuts</h3>
+            <ul>
+              <li>Align Left: CTRL + SHIFT + L</li>
+              <li>Align Center: CTRL + SHIFT + C</li>
+              <li>Align Right: CTRL + SHIFT + R</li>
+              <li>Align Justify: CTRL + SHIFT + J</li>
+              <li>Bold: CTRL + B</li>
+              <li>Divider: CTRL + ]</li>
+              <li>Italic: CTRL + I</li>
+              <li>Strikethrough: CTRL + SHIFT + S</li>
+              <li>Underline: CTRL + U</li>
+            </ul>
+            <h3>Found a bug? Got a request?</h3>
+            <p>
+              Post an issue in the{" "}
+              <em>
                 <a
                   className={classes.link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://github.com/dictyBase/migration-data/issues/9">
-                  this issue
+                  href="https://github.com/dictyBase/dicty-frontpage/issues">
+                  dicty-frontpage
                 </a>
-                .
-              </p>
-              <p>
-                <strong>Known bugs:</strong> Lists and tables sometimes do not
-                work as expected when pasted into the editor. For lists, your
-                best bet is to copy item by item (or a few items at once), then
-                format accordingly. For tables, it will be easier to create a
-                new table with the applicable toolbar buttons (i.e. create
-                table, add row, etc.), then paste in the content cell by cell.
-              </p>
-              <h3>Keyboard Shortcuts</h3>
-              <ul>
-                <li>Align Left: CTRL + SHIFT + L</li>
-                <li>Align Center: CTRL + SHIFT + C</li>
-                <li>Align Right: CTRL + SHIFT + R</li>
-                <li>Align Justify: CTRL + SHIFT + J</li>
-                <li>Bold: CTRL + B</li>
-                <li>Divider: CTRL + ]</li>
-                <li>Italic: CTRL + I</li>
-                <li>Strikethrough: CTRL + SHIFT + S</li>
-                <li>Underline: CTRL + U</li>
-              </ul>
-              <h3>Found a bug? Got a request?</h3>
-              <p>
-                Post an issue in the{" "}
-                <em>
-                  <a
-                    className={classes.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href="https://github.com/dictyBase/dicty-frontpage/issues">
-                    dicty-frontpage
-                  </a>
-                </em>{" "}
-                repo.
-              </p>
-              <SaveButton className={classes.save} onClick={handleClose}>
-                Close
-              </SaveButton>
-            </Grid>
+              </em>{" "}
+              repo.
+            </p>
           </Grid>
-        </ClickAwayListener>
+        </Grid>
       </DialogContent>
-    </Modal>
+      <DialogActions>
+        <SaveButton className={classes.save} onClick={handleClose}>
+          Close
+        </SaveButton>
+      </DialogActions>
+    </Dialog>
   )
 }
 
