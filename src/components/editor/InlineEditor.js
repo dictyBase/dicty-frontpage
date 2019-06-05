dfdfdd02f80214ceb416c53cd2da7a4656dc6cde
wildlifehexagon <crossthebreeze@gmail.com>
2019-06-05 11:50:03 -0500

    refactor: pare down the InlineEditor component

--- src/components/editor/InlineEditor.js
+++ src/components/editor/InlineEditor.js
@@ -1,20 +1,17 @@
 // @flow
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { Editor, getEventTransfer, getEventRange } from "slate-react"
+import { Editor } from "slate-react"
 import { Value } from "slate"
 import { withStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
 import CreateIcon from "@material-ui/icons/Create"
-
 import Authorization from "components/authentication/Authorization"
 import EditorToolbar from "./toolbar/EditorToolbar"
 import PageEditorBottomButtons from "./PageEditorBottomButtons"
 import renderMark from "./renderers/renderMark"
 import renderNode from "./renderers/renderNode"
 import schema from "./schema/schema"
-import { insertImage } from "./plugins/image"
-import { onPasteHtml, onPasteText } from "./utils/utils"
 import { editInline, saveInlineEditing } from "actions/editablePages"
 import styles from "./editorStyles"
 import placeholder from "./data/existingPagePlaceholder.json"
@@ -74,13 +71,14 @@ type State = {
 
 /**
  * This is a reusable Slate inline page editor component.
+ * It is a pared down version of the PageEditor component
+ * with specific inline editing functions and authorization.
  */
 
 class InlineEditor extends Component<Props, State> {
   editor: Ref // necessary for Flow
   constructor(props: Props) {
     super(props)
-
     if (props.page) {
       this.state = {
         // Initial value of editor
@@ -94,7 +92,6 @@ class InlineEditor extends Component<Props, State> {
         readOnly: props.readOnly,
       }
     }
-
     this.editor = React.createRef()
   }
 
@@ -125,7 +122,6 @@ class InlineEditor extends Component<Props, State> {
     const { page, saveInlineEditing, userId } = this.props
 
     const content = JSON.stringify(value.toJSON())
-
     const body = {
       id: page.data.id,
       data: {
@@ -138,45 +134,7 @@ class InlineEditor extends Component<Props, State> {
       },
     }
     saveInlineEditing(page.data.id, body)
-
     this.setState(value)
-  }
-
-  onPaste = (e: SyntheticEvent<>, editor, next) => {
-    const transfer = getEventTransfer(e)
-    const { type } = transfer
-    switch (type) {
-      case "text":
-        return onPasteText(e, this.editor.current, next)
-      case "html":
-        return onPasteHtml(e, this.editor.current, next)
-      default:
-        return next()
-    }
-  }
-
-  onDrop = (event, change, editor) => {
-    const target = getEventRange(event, change.value)
-    if (!target && event.type === "drop") return
-
-    const transfer = getEventTransfer(event)
-    const { type, files } = transfer
-
-    if (type === "files") {
-      for (const file of files) {
-        const reader = new FileReader()
-        const [mime] = file.type.split("/")
-        if (mime !== "image") continue
-
-        reader.addEventListener("load", () => {
-          editor.change(c => {
-            c.call(insertImage, reader.result, target)
-          })
-        })
-
-        reader.readAsDataURL(file)
-      }
-    }
   }
 
   render() {
@@ -190,8 +148,6 @@ class InlineEditor extends Component<Props, State> {
             className={classes.editor}
             value={value}
             onChange={this.onChange}
-            onPaste={this.onPaste}
-            onDrop={this.onDrop}
             renderMark={renderMark}
             renderNode={renderNode}
             readOnly={readOnly}
@@ -233,8 +189,6 @@ class InlineEditor extends Component<Props, State> {
           className={classes.editor}
           value={value}
           onChange={this.onChange}
-          onPaste={this.onPaste}
-          onDrop={this.onDrop}
           renderMark={renderMark}
           renderNode={renderNode}
           readOnly={readOnly}
