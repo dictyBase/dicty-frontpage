9abe78999b9fd4cf568eca62d96b5bd4847ef1be
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-13 12:40:46 -0500

    refactor: remove unused editablepage redux content

--- src/app/styleguide/mockReducers.js
+++ src/app/styleguide/mockReducers.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux"
 import downloadsReducer from "reducers/downloads"
-import editablePagesReducer from "reducers/editablePages"
 
 export default (history) =>
   combineReducers({
     downloads: downloadsReducer,
-    editablePages: editablePagesReducer,
   })
--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux"
 import { connectRouter } from "connected-react-router"
 import downloadsReducer from "common/reducers/downloads"
-import editablePagesReducer from "common/reducers/editablePages"
 
 export default (history) =>
   combineReducers({
     router: connectRouter(history),
     downloads: downloadsReducer,
-    editablePages: editablePagesReducer,
   })
