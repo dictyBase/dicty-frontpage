84a0f88ce74464f3476d2fea3ee1e4c28811760f
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-14 13:33:39 -0500

    feat: add query to get content by id

--- src/common/graphql/query.ts
+++ src/common/graphql/query.ts
@@ -25,6 +25,32 @@ const GET_CONTENT_BY_SLUG = gql`
   }
 `
 
+const GET_CONTENT_BY_ID = gql`
+  query content($id: ID!) {
+    contentByID(id: $id) {
+      id
+      content
+      name
+      slug
+      namespace
+      updated_by {
+        id
+        email
+        first_name
+        last_name
+        updated_at
+        roles {
+          role
+          permissions {
+            permission
+            resource
+          }
+        }
+      }
+    }
+  }
+`
+
 const GET_REFRESH_TOKEN = gql`
   query GetRefreshToken($token: String!) {
     getRefreshToken(token: $token) {
@@ -57,4 +83,9 @@ const GET_USER_BY_EMAIL = gql`
   }
 `
 
-export { GET_CONTENT_BY_SLUG, GET_REFRESH_TOKEN, GET_USER_BY_EMAIL }
+export {
+  GET_CONTENT_BY_SLUG,
+  GET_CONTENT_BY_ID,
+  GET_REFRESH_TOKEN,
+  GET_USER_BY_EMAIL,
+}
