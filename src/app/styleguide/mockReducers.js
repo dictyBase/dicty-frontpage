d728b43427f9e51519039501f9c6f57349f91d4d
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-18 16:07:42 -0500

    refactor: remove all redux content

--- src/app/styleguide/mockReducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux"
-import downloadsReducer from "reducers/downloads"
-
-export default (history) =>
-  combineReducers({
-    downloads: downloadsReducer,
-  })
--- src/common/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux"
-import { connectRouter } from "connected-react-router"
-import downloadsReducer from "common/reducers/downloads"
-
-export default (history) =>
-  combineReducers({
-    router: connectRouter(history),
-    downloads: downloadsReducer,
-  })
