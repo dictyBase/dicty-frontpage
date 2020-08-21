f153d477f50a5cdb993e6b37fc0c758c4f4a9f32
wildlifehexagon <crossthebreeze@gmail.com>
2020-08-21 15:17:40 -0500

    fix: remove basename from msg callback

--- src/features/Authentication/OauthCallback.tsx
+++ src/features/Authentication/OauthCallback.tsx
@@ -15,7 +15,7 @@ const OauthCallback = () => {
       {
         query: location.search,
         provider: provider,
-        url: `${window.location.origin}${process.env.REACT_APP_BASENAME}${location.pathname}`,
+        url: `${window.location.origin}${location.pathname}`,
       },
       window.location,
     )
