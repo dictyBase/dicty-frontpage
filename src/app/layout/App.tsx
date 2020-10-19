18f7683b7f9381dde8810b0511a43614ae9d4ac6
wildlifehexagon <crossthebreeze@gmail.com>
2020-10-19 14:32:24 -0500

    fix: remove extra fetching of refresh token

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -6,7 +6,7 @@ import { Navbar } from "dicty-components-navbar"
 import jwtDecode from "jwt-decode"
 import { IconProp } from "@fortawesome/fontawesome-svg-core"
 import { useFetchRefreshToken, useFooter, useNavbar } from "dicty-hooks"
-import { useAuthStore } from "features/Authentication/AuthStore"
+import { useAuthStore, ActionType } from "features/Authentication/AuthStore"
 import ErrorBoundary from "common/components/errors/ErrorBoundary"
 import {
   headerItems,
@@ -15,6 +15,7 @@ import {
 } from "common/utils/headerItems"
 import Routes from "app/routes/Routes"
 import { GET_REFRESH_TOKEN } from "common/graphql/query"
+import { User } from "common/types/user"
 
 const useStyles = makeStyles({
   main: {
@@ -40,6 +41,36 @@ const navTheme = {
   secondary: "#0059b3",
 }
 
+type RefreshTokenData = {
+  token: string
+  user: User
+  identity: {
+    provider: string
+  }
+}
+
+type Action = {
+  type: string
+  payload: {
+    provider: string
+    token: string
+    user: User
+  }
+}
+
+const updateToken = (
+  dispatch: (arg0: Action) => void,
+  data: RefreshTokenData,
+) =>
+  dispatch({
+    type: ActionType.UPDATE_TOKEN,
+    payload: {
+      provider: data.identity.provider,
+      token: data.token,
+      user: data.user,
+    },
+  })
+
 const getTokenIntervalDelayInMS = (token: string) => {
   if (token === "") {
     return
@@ -64,30 +95,34 @@ type HeaderItem = {
  */
 
 export const App = () => {
+  const [skip, setSkip] = React.useState(false)
   const [{ isAuthenticated, token }, dispatch] = useAuthStore()
   const { navbarData } = useNavbar()
   const { footerData } = useFooter()
   const classes = useStyles()
-  const { refetch } = useQuery(GET_REFRESH_TOKEN, {
+  const { loading, refetch, data } = useQuery(GET_REFRESH_TOKEN, {
     variables: { token: token },
     errorPolicy: "ignore",
+    skip, // only run query once
   })
   const interval = React.useRef(null)
   const delay = getTokenIntervalDelayInMS(token)
+
+  // set skip to true so the query is only run once
+  // then update the refresh token in our global state
+  React.useEffect(() => {
+    if (!loading && data && data.getRefreshToken) {
+      setSkip(true)
+      updateToken(dispatch, data.getRefreshToken)
+    }
+  }, [data, dispatch, loading])
 
   const fetchRefreshToken = React.useCallback(async () => {
     try {
       const res = await refetch({ token: token })
       if (res.data.getRefreshToken) {
         const { data } = res
-        dispatch({
-          type: "UPDATE_TOKEN",
-          payload: {
-            provider: data.getRefreshToken.identity.provider,
-            token: data.getRefreshToken.token,
-            user: data.getRefreshToken.user,
-          },
-        })
+        updateToken(dispatch, data.getRefreshToken)
       }
     } catch (error) {
       console.error(error)
@@ -113,4 +148,5 @@ export const App = () => {
   )
 }
 
+export { getTokenIntervalDelayInMS }
 export default App
