f34985ead4b58c4a10b2246732b93569bd815387
wildlifehexagon <crossthebreeze@gmail.com>
2020-11-20 17:05:58 -0600

    refactor: clean up linting issues

--- src/common/data/stockcenter.ts
+++ src/common/data/stockcenter.ts
@@ -1,4 +1,6 @@
-export default {
+const stockcenter = {
   plasmids: ["pTgrR1/LacZ", "pBeiB/YFP", "pSigK", "pExpl7/lacZ"],
   strains: ["Mad52", "grlD-/[act15]:grlD", "grlD-", "ase1A-"],
 }
+
+export default stockcenter
