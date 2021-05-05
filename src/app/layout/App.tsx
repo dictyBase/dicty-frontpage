56d77df2093533d80cb4c749c8d65da055123ce4
wildlifehexagon <hartline.eric@gmail.com>
2021-05-05 12:29:39 -0500

    refactor: convert to latest header-footer and dicty-hooks, fixes #753

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,37 +1,54 @@
 import React from "react"
 import { useQuery } from "@apollo/client"
-import { makeStyles } from "@material-ui/core/styles"
+import { makeStyles, Theme } from "@material-ui/core/styles"
+import Container from "@material-ui/core/Container"
 import { Header, Footer } from "dicty-components-header-footer"
 import { Navbar } from "dicty-components-navbar"
 import jwtDecode from "jwt-decode"
-import { IconProp } from "@fortawesome/fontawesome-svg-core"
-import { useFetchRefreshToken, useFooter, useNavbar } from "dicty-hooks"
+import { useFetchRefreshToken, useFetch } from "dicty-hooks"
 import { useAuthStore, ActionType } from "features/Authentication/AuthStore"
 import ErrorBoundary from "common/components/errors/ErrorBoundary"
 import {
   headerItems,
   loggedHeaderItems,
-  generateLinks,
+  HeaderLinks,
 } from "common/utils/headerItems"
+import {
+  footerLinks,
+  footerURL,
+  convertFooterData,
+  FooterItems,
+} from "common/utils/footerItems"
+import {
+  navbarItems,
+  NavbarItems,
+  navbarURL,
+  formatNavbarData,
+} from "common/utils/navbarItems"
+import { navTheme, headerTheme, footerTheme } from "common/utils/themes"
 import Routes from "app/routes/Routes"
 import { GET_REFRESH_TOKEN } from "common/graphql/query"
 import { User } from "common/types"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   main: {
-    marginBottom: "50px",
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
   },
   body: {
     fontSize: "16px",
     color: "#333",
     backgroundColor: "#fff",
+    "& h1, h2, h3, h4, h5, h6": {
+      fontWeight: 500,
+      lineHeight: 1.1,
+    },
+    "& h4, h5, h6": {
+      marginTop: theme.spacing(1.2),
+      marginBottom: theme.spacing(1.2),
+    },
   },
-})
-
-const navTheme = {
-  primary: "#004080",
-  secondary: "#0059b3",
-}
+}))
 
 type RefreshTokenData = {
   token: string
@@ -42,7 +59,7 @@ type RefreshTokenData = {
 }
 
 type Action = {
-  type: string
+  type: ActionType.UPDATE_TOKEN
   payload: {
     provider: string
     token: string
@@ -75,13 +92,6 @@ const getTokenIntervalDelayInMS = (token: string) => {
   return (timeDiffInMins - 2) * 60 * 1000
 }
 
-type HeaderItem = {
-  isRouter?: boolean
-  text: string
-  icon: IconProp
-  url: string
-}
-
 /**
  * App is responsible for the main layout of the entire application.
  */
@@ -89,8 +99,8 @@ type HeaderItem = {
 export const App = () => {
   const [skip, setSkip] = React.useState(false)
   const [{ isAuthenticated, token }, dispatch] = useAuthStore()
-  const { navbarData } = useNavbar()
-  const { footerData } = useFooter()
+  const navbar = useFetch<NavbarItems>(navbarURL, navbarItems)
+  const footer = useFetch<FooterItems>(footerURL, footerLinks)
   const classes = useStyles()
   const { loading, refetch, data } = useQuery(GET_REFRESH_TOKEN, {
     variables: { token: token },
@@ -126,16 +136,21 @@ export const App = () => {
 
   return (
     <div className={classes.body}>
-      <Header items={headerContent}>
-        {(items: Array<HeaderItem>) => items.map(generateLinks)}
-      </Header>
-      <Navbar items={navbarData} theme={navTheme} />
+      <Header items={headerContent} render={HeaderLinks} theme={headerTheme} />
+      <Navbar items={formatNavbarData(navbar.data)} theme={navTheme} />
       <main className={classes.main}>
-        <ErrorBoundary>
-          <Routes />
-        </ErrorBoundary>
+        <Container maxWidth="xl">
+          <ErrorBoundary>
+            <Routes />
+          </ErrorBoundary>
+        </Container>
       </main>
-      <Footer items={footerData} />
+      {footer.data?.data && (
+        <Footer
+          links={convertFooterData(footer.data.data)}
+          theme={footerTheme}
+        />
+      )}
     </div>
   )
 }
