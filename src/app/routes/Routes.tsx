8089e268788bba8dd7d494702b9400acbde4ab7e
wildlifehexagon <hartline.eric@gmail.com>
2021-05-05 12:17:22 -0500

    refactor: use custom hook for google analytics

--- src/app/routes/Routes.tsx
+++ src/app/routes/Routes.tsx
@@ -12,34 +12,43 @@ import AuthLoader from "features/Authentication/AuthLoader"
 import Logout from "features/Authentication/Logout"
 import PageNotReady from "common/components/PageNotReady"
 import PrivateRoute from "./PrivateRoute"
+import useGoogleAnalytics from "common/hooks/useGoogleAnalytics"
 
-const Routes = () => (
-  <Switch>
-    <Route exact path="/" component={Front} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/downloads" component={DownloadsContainer} />
-    {/* Authentication routes */}
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/:provider/callback" component={OauthCallback} />
-    <Route exact path="/load/auth" component={AuthLoader} />
-    <PrivateRoute exact path="/logout" component={Logout} />
-    {/* Editable page routes */}
-    <Route exact path="/:section/:name" component={InfoPageContainer} />
-    <PrivateRoute exact path="/:section/:name/edit" component={EditInfoPage} />
-    <Route
-      exact
-      path="/:section/:name/:subname"
-      component={InfoPageContainer}
-    />
-    <PrivateRoute
-      exact
-      path="/:section/:name/:subname/edit"
-      component={EditInfoPage}
-    />
-    <PrivateRoute exact path="/addpage" component={AddPage} />
-    {/* Page not found routes */}
-    <Route exact path="*" component={PageNotReady} />
-  </Switch>
-)
+const Routes = () => {
+  useGoogleAnalytics()
+
+  return (
+    <Switch>
+      <Route exact path="/" component={Front} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/downloads" component={DownloadsContainer} />
+      {/* Authentication routes */}
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/:provider/callback" component={OauthCallback} />
+      <Route exact path="/load/auth" component={AuthLoader} />
+      <PrivateRoute exact path="/logout" component={Logout} />
+      {/* Editable page routes */}
+      <Route exact path="/:section/:name" component={InfoPageContainer} />
+      <PrivateRoute
+        exact
+        path="/:section/:name/edit"
+        component={EditInfoPage}
+      />
+      <Route
+        exact
+        path="/:section/:name/:subname"
+        component={InfoPageContainer}
+      />
+      <PrivateRoute
+        exact
+        path="/:section/:name/:subname/edit"
+        component={EditInfoPage}
+      />
+      <PrivateRoute exact path="/addpage" component={AddPage} />
+      {/* Page not found routes */}
+      <Route exact path="*" component={PageNotReady} />
+    </Switch>
+  )
+}
 
 export default Routes
