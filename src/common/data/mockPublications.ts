ba0e78ee1da63d040241660baad2486a0e0d6207
Sean Kim <seank4@uic.edu>
2021-11-03 03:26:11 -0500

    Create mockPublications.ts
    mockPublications will be used to test Papers

---
+++ src/common/data/mockPublications.ts
@@ -0,0 +1,44 @@
+const listOfPublications = {
+    listPublications: [
+        {
+            id: '1',
+            title: 'Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.',
+            journal: 'Cells 8',
+            source: 'http://localhost:3000/publication/31108912',
+            authors: {
+              last_name: 'Tanaka, Jahan, Kondo, Nakano & Yumura (2019)'
+            }
+        },
+        {
+            id: '2',
+            title: 'Force balances between interphase centrosomes, as revealed by laser ablation.',
+            journal: 'Mol. Biol. Cell mbcE19010034',
+            source: 'http://localhost:3000/publication/31067156',
+            authors: {
+              last_name: 'Odell, Sikirzhytski, Tikhonenko, Cobani, Khodjakov & Koonce (2019)'
+            }
+        },
+        {
+            id: '3',
+            title: 'A terpene synthase-cytochrome P450 cluster in Dictyostelium discoideum produces a novel trisnorsesquiterpene.',
+            journal: 'Elife 8',
+            source: 'http://localhost:3000/publication/31063135',
+            authors: {
+              last_name: 'Chen, Luck, Rabe, Dinh, Shaulsky, Nelson, Gershenzon, Dickschat, Kollner & Chen (2019)'
+            }
+        },
+        {
+            id: '4',
+            title: 'Cyclic AMP induction of Dictyostelium prespore gene expression requires autophagy.',
+            journal: 'Dev. Biol.',
+            source: 'http://localhost:3000/publication/31051160',
+            authors: {
+              last_name: 'Yamada & Schaap (2019)'
+            }
+        },
+    ]
+}
+
+export {
+    listOfPublications
+}
