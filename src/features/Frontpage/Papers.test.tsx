60cc7918bb6095549b13fb75bc0d845affdba911
Sean Kim <seank4@uic.edu>
2021-11-05 01:52:33 -0500

    test: create test for graphql error handling

--- src/features/Frontpage/Papers.test.tsx
+++ src/features/Frontpage/Papers.test.tsx
@@ -1,63 +1,74 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
+import TestRenderer from 'react-test-renderer';
 import { InMemoryCache } from "@apollo/client"
-import { MockedProvider } from "@apollo/client/testing"
 import { BrowserRouter } from "react-router-dom"
 import Papers from "./Papers"
 import { listOfPublications } from "common/data/mockPublications"
-// import {  } from "dicty-graphql-schema"
-import {listOfPublications} from "../../common/data/mockPublications"
+import { ListRecentPublicationsDocument, useListRecentPublicationsQuery } from "dicty-graphql-schema"
+import { renderHook } from "@testing-library/react-hooks" 
+import { MockedProvider, MockedResponse } from "@apollo/client/testing"
 
-const mockParams = "abolished+protein+phosphorylation"
 
-// https://stackoverflow.com/questions/58117890/how-to-test-components-using-new-react-router-hooks
-jest.mock("react-router-dom", () => {
-  const originalModule = jest.requireActual("react-router-dom")
-  return {
-    ...originalModule,
-    useParams: () => ({
-      name: mockParams,
-    }),
-  }
-})
 
 describe("features/Frontpage/Papers", () => {
-    const window = global as any
-    let mockObserve: jest.Mock
-    let mockDisconnect: jest.Mock
-    beforeEach(() => {
-      mockObserve = jest.fn()
-      mockDisconnect = jest.fn()
-    })
-    beforeAll(() => {
-      window.IntersectionObserver = jest.fn((callback, options) => ({
-        observe: mockObserve,
-        disconnect: mockDisconnect,
-      }))
-      jest.setTimeout(30000)
-    })
   
-    describe("initial render with small data set", () => {
-        const mocks = [
-            {
-            request: {
-                query: getListOfPublications
-            },
-            result: {
-                data: {
-                    listPublications: listOfPublications.listPublications
-                },
-            },
-            },
-        ]
-        describe("Initial render with mock data", () => {
-            it("should render fetched data", async () => {
-                render(
-                <MockedProvider mocks={mocks} addTypename={false}>
-                    <BrowserRouter>
-                        <Papers papers={[mocks]} />
-                    </BrowserRouter>
-                </MockedProvider>,
-                )
-        })
-})
+  const mocks =  {
+    request: {
+        query: ListRecentPublicationsDocument,
+        variables: 4
+    },
+    result: {
+        data: {
+            listPublications: listOfPublications.listPublications
+        },
+    }
+  }
+  
+  const ListRecentPublicationsErrorMock = {
+      request: {
+        query: ListRecentPublicationsDocument,
+        variables: 4
+      },
+      error: new Error("GraphQL Query Failed")
+    }
+
+
+  const getHookWrapper = (mocks:any) => {
+    const wrapper = ({ children }: any) => (
+      <MockedProvider mocks={mocks} addTypename={false}>
+        {children}
+      </MockedProvider>
+    );
+    const { result, waitForNextUpdate } = renderHook(() => useListRecentPublicationsQuery(), {
+    wrapper
+    });
+    // Test the initial state of the request
+    expect(result.current.loading).toBeTruthy();
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.data?.listRecentPublications).toBeUndefined();
+    return { result, waitForNextUpdate };
+  }
+
+  // it("Renders fetched data", async () => {
+  //   const { result, waitForNextUpdate } = getHookWrapper([mocks]);
+
+  //   await waitForNextUpdate();
+  //   console.log(result);
+
+  //   expect(result.current.loading).toBeFalsy();
+  //   expect(result.current.error).toBeUndefined();
+  //   // expect(result.current.data?.listRecentPublications).toEqual();
+  // });
+  
+  it("Handle Errors as expected", async () => {
+    const { result, waitForNextUpdate } = getHookWrapper([ListRecentPublicationsErrorMock]);
+
+    await waitForNextUpdate();
+
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.error).toBeTruthy();
+    expect(result.current.data?.listRecentPublications).toBeUndefined();
+  });
+
+  })
