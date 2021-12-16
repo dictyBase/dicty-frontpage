7c26e8e27db5a6af170f7e299a12b583c38277d6
Sean Kim <seank4@uic.edu>
2021-12-16 10:37:01 -0600

    fix: remove unused definitions

--- src/features/EditablePages/InlineEditor.test.tsx
+++ src/features/EditablePages/InlineEditor.test.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { render, screen, act } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import waitForExpect from "wait-for-expect"
 import InlineEditor from "./InlineEditor"
