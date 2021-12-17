203820da3ef9922dc3713c5d994362617a23852c
Sean Kim <seank4@uic.edu>
2021-12-17 13:16:13 -0600

    feat: add env variables for msw

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -12,6 +12,8 @@ declare global {
       REACT_APP_GRAPHQL_SERVER: string
       // the alternate graphql server used to prevent cross-site cookie issues
       REACT_APP_ALT_GRAPHQL_SERVER: string
+      // Activates mock service worker
+      REACT_APP_MOCK_SERVER: "on" | "off"
       // used to differentiate between deploy environments, specifically for using correct GraphQL URL
       DEPLOY_ENV: "development" | "staging" | "production"
     }
