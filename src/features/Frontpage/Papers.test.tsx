b5bc3762e392667f2ead039e88f6a50d3141ebce
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2022-01-05 11:02:02 -0600

    test: return proper mock data for mock useListRecentPublicationQuery hook

--- src/features/Frontpage/Papers.test.tsx
+++ src/features/Frontpage/Papers.test.tsx
@@ -1,11 +1,7 @@
-import React from "react"
 import { render, screen } from "@testing-library/react"
-import { ApolloProvider } from '@apollo/client'
-import { client } from './ApolloPapersClient'
 import Papers from "./Papers"
 import { useListRecentPublicationsQuery } from "dicty-graphql-schema"
-import { listRecentPublications } from '../../common/data/mockPublications'
-
+import { listRecentPublications } from "../../common/data/mockPublications"
 
 jest.mock("dicty-graphql-schema", () => {
   const useListRecentPublicationsQuery = jest.fn()
@@ -19,16 +15,20 @@ describe("feature/Frontpage/Papers Browser based", () => {
     ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
       loading: false,
       error: false,
-      data: listRecentPublications,
-      })
-      render(<Papers/>)
+      data: { listRecentPublications: [...listRecentPublications] },
+    })
+    render(<Papers />)
 
-      expect(screen.getByText("Tanaka, Jahan, Kondo, Nakano & Yumura (2019)")).toBeInTheDocument()
-      expect(screen.getByText("Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.")).toBeInTheDocument()
-      expect(screen.getByText("Cells 8")).toBeInTheDocument()
+    expect(
+      screen.getByText("Tanaka, Jahan, Kondo, Nakano & Yumura (2019)"),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        "Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.",
+      ),
+    ).toBeInTheDocument()
+    expect(screen.getByText("Cells 8")).toBeInTheDocument()
   })
-
-
 })
 
 // describe("features/Frontpage/Papers", () => {
@@ -48,16 +48,16 @@ describe("feature/Frontpage/Papers Browser based", () => {
 //     expect(author).toHaveTextContent('Tanaka, Jahan, Kondo, Nakano & Yumura (2019)')
 //     expect(title).toHaveTextContent('Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.')
 //     expect(journal).toHaveTextContent('Cells 8')
-  
+
 //     /* Check to see that the rest of the Journals loaded in */
 //     const author2 = await screen.findByTestId('paper-author-1')
 //     const author3 = await screen.findByTestId('paper-author-2')
 //     const author4 = await screen.findByTestId('paper-author-3')
-    
+
 //     expect(author2).toBeInTheDocument();
 //     expect(author3).toBeInTheDocument();
 //     expect(author4).toBeInTheDocument();
 
 //   })
-  
+
 // })
