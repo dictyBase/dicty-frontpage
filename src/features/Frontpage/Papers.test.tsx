f20ee10ed18b8a6e19b61cb09ec040271bbcd0e4
Sean Kim <seank4@uic.edu>
2022-01-05 11:07:19 -0600

    test: add comprehensive testing to Papers

--- src/features/Frontpage/Papers.test.tsx
+++ src/features/Frontpage/Papers.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react"
 import Papers from "./Papers"
 import { useListRecentPublicationsQuery } from "dicty-graphql-schema"
 import { listRecentPublications } from "../../common/data/mockPublications"
+import { ApolloError } from "@apollo/client"
 
 jest.mock("dicty-graphql-schema", () => {
   const useListRecentPublicationsQuery = jest.fn()
@@ -29,35 +30,19 @@ describe("feature/Frontpage/Papers Browser based", () => {
     ).toBeInTheDocument()
     expect(screen.getByText("Cells 8")).toBeInTheDocument()
   })
+
+  it("should render loading", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      loading: true
+    })
+    render(<Papers />)
+    expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+  })
+
+  it("should render Apollo Error Component", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      error: new ApolloError({})
+    })
+    render(<Papers />)
+  })
 })
-
-// describe("features/Frontpage/Papers", () => {
-
-//   it('render publications', async () => {
-//     render(
-//       <ApolloProvider client={client}>
-//         <Papers />
-//       </ApolloProvider>
-//     )
-
-//     /* Check that the first Journal is being shown correctly */
-//     const author = await screen.findByTestId('paper-author-0')
-//     const title = await screen.findByTestId('paper-title-0')
-//     const journal = await screen.findByTestId('paper-journal-0')
-
-//     expect(author).toHaveTextContent('Tanaka, Jahan, Kondo, Nakano & Yumura (2019)')
-//     expect(title).toHaveTextContent('Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.')
-//     expect(journal).toHaveTextContent('Cells 8')
-
-//     /* Check to see that the rest of the Journals loaded in */
-//     const author2 = await screen.findByTestId('paper-author-1')
-//     const author3 = await screen.findByTestId('paper-author-2')
-//     const author4 = await screen.findByTestId('paper-author-3')
-
-//     expect(author2).toBeInTheDocument();
-//     expect(author3).toBeInTheDocument();
-//     expect(author4).toBeInTheDocument();
-
-//   })
-
-// })
