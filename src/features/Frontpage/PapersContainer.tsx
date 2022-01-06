6b3576f6f06cf96e337d9c90c28c802d5bcda6a6
Sean Kim <seank4@uic.edu>
2022-01-06 14:06:07 -0600

    refactor: Move GraphQL queries into PapersContainer

--- src/features/Frontpage/PapersContainer.tsx
+++ src/features/Frontpage/PapersContainer.tsx
@@ -5,20 +5,19 @@ import Loader from "common/components/Loader"
 import Papers from "./Papers"
 
 const PapersContainer = () => {
+  let { loading, error, data } = useListRecentPublicationsQuery({
+    variables: {
+      limit: 4,
+    },
+  })
 
-    let { loading, error, data } = useListRecentPublicationsQuery({
-        variables: {
-            limit: 4
-        },
-    });
-
-    return (
-        <>
-            {loading ? <Loader /> : <></>}
-            {error ? <GraphQLErrorPage error={error} /> : <></>}
-            {data ? <Papers data={data}/> : <></>}
-        </>
-    )
+  return (
+    <>
+      {loading ? <Loader /> : <></>}
+      {error ? <GraphQLErrorPage error={error} /> : <></>}
+      {data ? <Papers data={data} /> : <></>}
+    </>
+  )
 }
 
 export default PapersContainer
