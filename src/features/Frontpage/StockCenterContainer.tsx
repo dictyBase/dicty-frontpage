c4d3cb07c67bd64bdd85abcd9b6255ffa2bbd302
Sean Kim <seank4@uic.edu>
2022-01-09 23:40:42 -0600

    refactor: Change StockCenterContainer to pass Graphql data to StockCenter

--- src/features/Frontpage/StockCenterContainer.tsx
+++ src/features/Frontpage/StockCenterContainer.tsx
@@ -1,156 +1,32 @@
 import React from "react"
-import { makeStyles } from "@material-ui/core/styles"
-import Grid from "@material-ui/core/Grid"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useListRecentPlasmidsQuery, useListRecentStrainsQuery } from "dicty-graphql-schema"
 import Loader from "common/components/Loader"
 import GraphQLErrorPage from "common/components/errors/GraphQLErrorPage"
 import StockCenter from "./StockCenter"
 
-const useStyles = makeStyles({
-  listItem: {
-    listStyle: "none",
-    fontSize: "12px",
-  },
-  listBox: {
-    padding: "0px 25px 10px 25px",
-    fontSize: "12px",
-    marginBottom: "5px",
-    paddingBottom: "0px",
-    marginTop: "5px",
-    "@media (max-width: 992px) and (min-width: 767px)": {
-      fontSize: "10px",
-    },
-    "@media (max-width: 768px)": {
-      fontSize: "16px",
-    },
-  },
-  plusSign: {
-    color: "#0b3861",
-    fontSize: "11px",
-    fontStyle: "italic",
-    fontWeight: "normal",
-    textAlign: "center",
-    paddingBottom: "0px",
-
-    "@media (min-width: 1400px)": {
-      paddingTop: "30px",
-      fontSize: "12px",
-    },
-  },
-  title: {
-    paddingTop: "5px",
-    paddingLeft: "5px",
-  },
-  subheader: {
-    backgroundColor: "#effbfb",
-    color: "black",
-    fontSize: "12px",
-    letterSpacing: "5px",
-    padding: "1px 0px 1px 0px",
-
-    "@media (max-width: 767px)": {
-      fontSize: "12px",
-      textAlign: "center",
-    },
-  },
-  header: {
-    backgroundColor: "#004080",
-    color: "#efeffb",
-    fontSize: "18px",
-    textAlign: "center",
-    padding: "10px 0px 10px 0px",
-    border: "1px dotted #f5f6ce",
-    borderTopLeftRadius: "4px",
-    borderTopRightRadius: "4px",
-    "@media (max-width: 767px)": {
-      fontSize: "18px",
-      textAlign: "center",
-    },
-  },
-  container: {
-    textAlign: "center",
-    padding: "5px 5px 0 5px",
-    marginBottom: "10px",
-  },
-  plasmidBox: {
-    color: "#fff",
-    backgroundColor: "#0073e6",
-    textAlign: "center",
-    paddingBottom: "5px",
-    borderBottomLeftRadius: "4px",
-    borderBottomRightRadius: "4px",
-    "@media (max-width: 992px) and (min-width: 767px)": {
-      fontSize: "10px",
-    },
-    "@media (max-width: 767px)": {
-      fontSize: "12px",
-    },
-  },
-  strainBox: {
-    color: "#242124",
-    backgroundColor: "#80c1ff",
-    textAlign: "center",
-    paddingBottom: "5px",
-    borderBottomLeftRadius: "4px",
-    borderBottomRightRadius: "4px",
-    "@media (max-width: 992px) and (min-width: 767px)": {
-      fontSize: "10px",
-    },
-    "@media (max-width: 767px)": {
-      fontSize: "12px",
-    },
-  },
-})
-
 /** Widget that displays the most recent plasmids and strains in the Stock Center */
 const StockCenterContainer = () => {
-    const classes = useStyles()
 
-    let plasmidList;
     const { data: plasmidData, loading: plasmidLoading, error: plasmidError } = useListRecentPlasmidsQuery({
         variables: {
             limit: 4
         },
     });
 
-    let strainList;
     const { data: strainData, loading: strainLoading, error: strainError } = useListRecentStrainsQuery({
         variables: {
             limit: 4
         },
     });
 
-    if(plasmidData) { 
-        plasmidList = plasmidData?.listRecentPlasmids?.map((plasmid, index) => {
-            return (
-            )
-        })
-    }
-
-    if(strainData) {
-        strainList = strainData?.listRecentStrains?.map((strain, index) => {
-            return (
-            )
-        })
-    }
-
-    let text
-    if(strainList && plasmidList) {
-        let temp = <div className={classes.plusSign}><FontAwesomeIcon icon="plus" /></div>
-        text = strainList.concat(temp)
-        text = text.concat(plasmidList)
-        text.concat(temp)
-    }
-
-    return (
-        <StockCenter>
-            {(plasmidLoading || strainLoading) ? <Loader /> : <></>}
-            {plasmidError ? <GraphQLErrorPage error={plasmidError} /> : <></>}
-            {strainError ? <GraphQLErrorPage error={strainError} /> : <></>}
-            {(strainData && plasmidData) ? text : <></>}
-        </StockCenter>
-    )
+  return (
+    <>
+      {(plasmidLoading || strainLoading) ? <Loader /> : <></>}
+      {plasmidError ? <GraphQLErrorPage error={plasmidError} /> : <></>}
+      {strainError ? <GraphQLErrorPage error={strainError} /> : <></>}
+      {(strainData && plasmidData) ? <StockCenter plasmidData={plasmidData} strainData={strainData} />: <></>}
+    </>
+  )
 }
 
 export default StockCenterContainer
