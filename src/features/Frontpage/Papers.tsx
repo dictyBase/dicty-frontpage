d42165d1df62ba71650bb4307d2054af17cf2109
Sean Kim <thesean.kim33@gmail.com>
2022-01-09 23:27:37 -0600

    Revert "Refactor/papers component (#1124)"
    This reverts commit 1cae9f4a7363bde01ce4f9464c541690bcdfc537.

--- src/features/Frontpage/Papers.tsx
+++ src/features/Frontpage/Papers.tsx
@@ -1,8 +1,7 @@
-import React from "react"
+import React, { ReactFragment } from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import Grid from "@material-ui/core/Grid"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { ListRecentPublicationsQuery } from "dicty-graphql-schema"
 
 const useStyles = makeStyles({
   container: {
@@ -51,25 +50,6 @@ const useStyles = makeStyles({
       fontSize: "16px",
     },
   },
-  listItem: {
-    listStyle: "none",
-    marginBottom: "10px",
-  },
-  leadText: {
-    color: "#0b3861",
-    paddingRight: "10px",
-  },
-  mainContent: {
-    paddingRight: "10px",
-  },
-  sourceContent: {
-    color: "#0b3861",
-  },
-  sourceTitle: {
-    paddingTop: "7px",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
   link: {
     textDecoration: "none",
     color: "#428bca",
@@ -90,55 +70,12 @@ const useStyles = makeStyles({
 })
 
 interface PaperContainerProps {
-  data: ListRecentPublicationsQuery | undefined
+  children: JSX.Element | JSX.Element[] | ReactFragment
 }
 
 /** Widget that displays the latest Dicty papers */
-const Papers = ({data}:PaperContainerProps):JSX.Element => {
+const Papers = ({children}:PaperContainerProps):JSX.Element => {
   const classes = useStyles();
-
-  let text;
-  if(data) {
-    text = data?.listRecentPublications?.map((paper, index) => {
-    const authors = paper?.authors
-    const doi = paper?.doi
-    let lastname;
-    if (!authors) return <></>
-    if (Array.isArray(authors[0]?.last_name)) {
-        lastname = (authors[0]?.last_name)?.join(", ")
-    } else {
-        lastname = authors[0]?.last_name
-    }
-
-    return (
-        <li className={classes.listItem} key={index}>
-        <span
-            data-testid={"paper-author-" + index}
-            className={classes.leadText}>
-            {lastname ? lastname : ""}
-        </span>
-        <span className={classes.mainContent}>
-        <strong>
-            <em data-testid={"paper-title-" + index}>{paper.title}</em>
-        </strong>
-        </span>
-        <br />
-        <span className={classes.sourceContent}>
-        <span className={classes.sourceTitle}>Journal: </span>
-        <span data-testid={"paper-journal-" + index}>{paper.journal}</span>
-        <a
-            className={classes.link}
-            href={doi ? doi : ""}
-            target="_blank"
-            rel="noopener noreferrer">
-            {" "}
-            Pubmed
-        </a>
-        </span>
-        </li>
-    )
-    })
-  }
 
   return (
     <div className={classes.container}>
@@ -150,7 +87,7 @@ const Papers = ({data}:PaperContainerProps):JSX.Element => {
           <span className={classes.title}> LATEST PAPERS</span>
         </Grid>
       </div>
-      <ul className={classes.listBox}>{text}</ul>
+      <ul className={classes.listBox}>{children}</ul>
       <div className={classes.bottomLink}>
         {/* <FontAwesome name="plus" />
         <Link to="/papers" alt="more papers">
