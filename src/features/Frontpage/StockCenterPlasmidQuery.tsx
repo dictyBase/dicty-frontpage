03531fe03b4568c300851021247ab9fbcca0b317
Sean Kim <seank4@uic.edu>
2022-01-12 11:24:06 -0600

    refactor: Move graphql queries into subcomponents of StockCenter

---
+++ src/features/Frontpage/StockCenterPlasmidQuery.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import { useListRecentPlasmidsQuery } from "dicty-graphql-schema"
+import Loader from "common/components/Loader"
+import GraphQLErrorPage from "common/components/errors/GraphQLErrorPage"
+import StockCenterItem from "./StockCenterItem"
+
+/** Widget that displays the most recent plasmids and strains in the Stock Center */
+const StockCenterPlasmidQuery = () => {
+  const { data, loading, error } = useListRecentPlasmidsQuery({
+    variables: {
+      limit: 4,
+    },
+  })
+
+  return (
+    <>
+      {loading ? <Loader /> : <></>}
+      {error ? <GraphQLErrorPage error={error} /> : <></>}
+      {data ? <StockCenterItem data={data} type={"Plasmid"} /> : <></>}
+    </>
+  )
+}
+
+export default StockCenterPlasmidQuery
