bed2bd12bc5cbb377f79bca0400d1dea027aa346
Sean Kim <seank4@uic.edu>
2022-01-13 17:14:40 -0600

    refactor: Update test cases

---
+++ src/features/Frontpage/PapersQuery.test.tsx
@@ -0,0 +1,84 @@
+import { render, screen } from "@testing-library/react"
+import PapersQuery from "./PapersQuery"
+import { useListRecentPublicationsQuery } from "dicty-graphql-schema"
+import { listRecentPublications } from "../../common/data/mockPublications"
+import { ApolloError } from "@apollo/client"
+
+jest.mock("dicty-graphql-schema", () => {
+  const useListRecentPublicationsQuery = jest.fn()
+  return {
+    useListRecentPublicationsQuery,
+  }
+})
+describe("feature/Frontpage/PaperQuery", () => {
+  beforeEach(() => jest.clearAllMocks())
+
+  it("should render publications in Annotations", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      loading: false,
+      error: false,
+      data: { listRecentPublications: [...listRecentPublications] },
+    })
+    render(<PapersQuery parent={"Annotations"} />)
+
+    /* Check if it renders Papers */
+    expect(screen.getByText("31108912")).toBeInTheDocument()
+    expect(screen.getByText("31067156")).toBeInTheDocument()
+    expect(screen.getByText("31063135")).toBeInTheDocument()
+  })
+
+  it("should render loading if publications is loading in Annotations", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      loading: true,
+    })
+    render(<PapersQuery parent={"Annotations"} />)
+
+    expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+  })
+
+  it("should render Apollo Error Component if publications failed to load in Annotations", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      error: new ApolloError({}),
+    })
+    render(<PapersQuery parent={"Annotations"} />)
+  })
+})
+
+describe("feature/Frontpage/Papers Browser based", () => {
+  beforeEach(() => jest.clearAllMocks())
+
+  it("should render papers", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      loading: false,
+      error: false,
+      data: { listRecentPublications: [...listRecentPublications] },
+    })
+    render(<PapersQuery parent={"Papers"} />)
+
+    expect(
+      screen.getByText("Tanaka, Jahan, Kondo, Nakano & Yumura (2019)"),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByText(
+        "Cytokinesis D is Mediated by Cortical Flow of Dividing Cells Instead of Chemotaxis.",
+      ),
+    ).toBeInTheDocument()
+    expect(screen.getByText("Cells 8")).toBeInTheDocument()
+  })
+
+  it("should render loading", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      loading: true,
+    })
+    render(<PapersQuery parent={"Papers"} />)
+
+    expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
+  })
+
+  it("should render Apollo Error Component", () => {
+    ;(useListRecentPublicationsQuery as jest.Mock).mockReturnValue({
+      error: new ApolloError({}),
+    })
+    render(<PapersQuery parent={"Papers"} />)
+  })
+})
