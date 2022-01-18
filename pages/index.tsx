bf7643f161eca1d895d4972f2f6502e3c8aeb2e7
Sean Kim <seank4@uic.edu>
2022-01-18 03:00:56 -0600

    refactor: Create nextjs index

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import "common/utils/polyfills"
+import React from "react"
+import ReactDOM from "react-dom"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { AuthProvider } from "./features/Authentication/AuthStore"
+import App from "./app/layout/App"
+import AppProviders from "./app/layout/AppProviders"
+import "common/utils/icons" // fontawesome library
+import "fontsource-roboto"
+
+const main = async () => {
+  if (process.env.NODE_ENV === "development") {
+    // Activate MSW
+    if (process.env.REACT_APP_MOCK_SERVER === "on") {
+      const { worker } = require("./mocks/browser.js")
+      await worker.start()
+    }
+  }
+}
+
+ReactDOM.render(
+  <AuthProvider>
+    <AppProviders>
+      <CssBaseline />
+      <App />
+    </AppProviders>
+  </AuthProvider>,
+  document.getElementById("root"),
+)
+
+main()
