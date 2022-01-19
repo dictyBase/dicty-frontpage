163e1d4356d04dc5629311a8d53fc5418c9f305a
Sean Kim <seank4@uic.edu>
2022-01-19 12:25:56 -0600

    refactor: update types to nextjs format

--- additional.d.ts
+++ additional.d.ts
@@ -1,19 +1,18 @@
-/// <reference types="react-scripts" />
+/// <reference types="next" />
 import "typescript"
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      NODE_ENV: "development" | "production"
-      REACT_APP_BASENAME: string
-      REACT_APP_GA_TRACKING_ID: string
-      REACT_APP_NAVBAR_JSON: string
-      REACT_APP_FOOTER_JSON: string
-      REACT_APP_GRAPHQL_SERVER: string
+      NEXT_PUBLIC_BASENAME: string
+      NEXT_PUBLIC_GA_TRACKING_ID: string
+      NEXT_PUBLIC_NAVBAR_JSON: string
+      NEXT_PUBLIC_FOOTER_JSON: string
+      NEXT_PUBLIC_GRAPHQL_SERVER: string
       // the alternate graphql server used to prevent cross-site cookie issues
-      REACT_APP_ALT_GRAPHQL_SERVER: string
+      NEXT_PUBLIC_ALT_GRAPHQL_SERVER: string
       // Activates mock service worker
-      REACT_APP_MOCK_SERVER: "on" | "off"
+      NEXT_PUBLIC_MOCK_SERVER: "on" | "off"
       // used to differentiate between deploy environments, specifically for using correct GraphQL URL
       DEPLOY_ENV: "development" | "staging" | "production"
     }
