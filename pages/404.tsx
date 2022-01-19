e4664822434380973bc8a9d03eb0b38edbb1e01e
Sean Kim <seank4@uic.edu>
2022-01-19 13:11:53 -0600

    feat: Handle 404 errors with 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+import PageNotReady from "../common/components/PageNotReady"
+
+/* This page allows us to create custom 404 errors */
+const Custom404 = () => {
+  return <PageNotReady />
+}
+
+export default Custom404
