a32f5483185b8388a39c70739287c17aebd18ef0
Sean Kim <seank4@uic.edu>
2022-01-19 11:27:14 -0600

    refactor: move meta tag to _app and remove index.html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,32 @@
 import type { AppProps } from "next/app"
 import { AuthProvider } from "../components/features/Authentication/AuthStore"
-import AppProviders from "./app/layout/AppProviders"
-import App from "./app/layout/App"
+import AppProviders from "../components/layout/AppProviders"
+import App from "../components/layout/App"
 import CssBaseline from "@material-ui/core/CssBaseline"
+import Head from "next/head"
 
 const DictyFrontPageApp = ({ Component, pageProps }: AppProps) => {
   return (
     <AuthProvider>
       <AppProviders>
         <CssBaseline />
-        <App />
+        <App>
+          <Head>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta name="theme-color" content="#000000" />
+            <meta
+              name="description"
+              content="The official homepage for dictyBase, your central resource for Dictyostelid genomics."
+            />
+            <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+            <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico" />
+            <title>dictyBase</title>
+          </Head>
+        </App>
         <Component {...pageProps} />
       </AppProviders>
     </AuthProvider>
