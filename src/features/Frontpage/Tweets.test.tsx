7d02595796182b37737935d5cd2c2a697e9d765b
Sean Kim <thesean.kim33@gmail.com>
2022-04-27 02:59:56 -0500

    feat: add test for dateFormatter in Tweets

---
+++ src/features/Frontpage/Tweets.test.tsx
@@ -0,0 +1,16 @@
+import { render } from "@testing-library/react"
+import { Tweets, dateFormatter } from "./Tweets"
+
+describe("feature/Frontpage/Tweets", () => {
+  beforeAll(() => {
+    jest.useFakeTimers("modern")
+    jest.setSystemTime(new Date(2022, 3, 27))
+  })
+
+  render(<Tweets />)
+
+  test("dateFormatter should return correct date format", () => {
+    /* I believe that it returning a day before is due to a timezone issue? */
+    expect(dateFormatter(new Date())).toBe("April 27, 2022")
+  })
+})
