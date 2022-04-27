25d23b49a3b554dfdd2d67754d81f53c51de5aca
Sean Kim <thesean.kim33@gmail.com>
2022-04-27 04:29:18 -0500

    Revert "feat: add PaperType to PaperItems"
    This reverts commit 1b09b90a230c8671469a5a5b7d1ac224440af489.

--- src/features/Frontpage/PapersItem.tsx
+++ src/features/Frontpage/PapersItem.tsx
@@ -32,60 +32,44 @@ interface PaperContainerProps {
   data: ListRecentPublicationsQuery | undefined
 }
 
-interface PaperType {
-  id: String
-  doi: String
-  title: String
-  abstract: String
-  journal: String
-  pub_date: String
-  pages: String
-  issue: String
-  volume: String
-  authors: [
-    {
-      initials: Array<String>
-      last_name: Array<String>
-    },
-  ]
-}
-
 const PapersItem = ({ data }: PaperContainerProps) => {
   const classes = useStyles()
 
-  let text = data?.listRecentPublications?.map(
-    (paper: PaperType, index: React.Key) => {
-      const authors = paper?.authors
-      const doi = paper?.doi
-      let lastname
-      if (!authors) return <></>
+  let text = data?.listRecentPublications?.map((paper, index) => {
+    const authors = paper?.authors
+    const doi = paper?.doi
+    let lastname
+    if (!authors) return <></>
+    if (Array.isArray(authors[0]?.last_name)) {
       lastname = authors[0]?.last_name?.join(", ")
+    } else {
+      lastname = authors[0]?.last_name
+    }
 
-      return (
-        <li className={classes.listItem} key={index}>
-          <span className={classes.leadText}>{lastname ? lastname : ""}</span>
-          <span className={classes.mainContent}>
-            <strong>
-              <em>{paper.title}</em>
-            </strong>
-          </span>
-          <br />
-          <span className={classes.sourceContent}>
-            <span className={classes.sourceTitle}>Journal:{"\u00A0"}</span>
-            <span>{paper.journal}</span>
-            <a
-              className={classes.link}
-              href={doi ? doi : ""}
-              target="_blank"
-              rel="noopener noreferrer">
-              {"\u00A0"}
-              Pubmed
-            </a>
-          </span>
-        </li>
-      )
-    },
-  )
+    return (
+      <li className={classes.listItem} key={index}>
+        <span className={classes.leadText}>{lastname ? lastname : ""}</span>
+        <span className={classes.mainContent}>
+          <strong>
+            <em>{paper.title}</em>
+          </strong>
+        </span>
+        <br />
+        <span className={classes.sourceContent}>
+          <span className={classes.sourceTitle}>Journal:{"\u00A0"}</span>
+          <span>{paper.journal}</span>
+          <a
+            className={classes.link}
+            href={doi ? doi : ""}
+            target="_blank"
+            rel="noopener noreferrer">
+            {"\u00A0"}
+            Pubmed
+          </a>
+        </span>
+      </li>
+    )
+  })
   return <>{text}</>
 }
 
