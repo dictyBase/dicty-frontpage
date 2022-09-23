2af52a40e518a24d82cc857a05338355923f7622
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-23 16:52:53 -0500

    build(vite): add plugin for static type checking/linting

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
 import path from "path"
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import checker from "vite-plugin-checker"
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), checker({ overlay: false, typescript: true })],
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "src"),
+        find: "common",
+        replacement: path.resolve(__dirname, "./src/common"),
       },
     },
     server: {
