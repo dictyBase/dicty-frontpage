40bd7ae0c309c3135241f2678567eb17a96688ce
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-26 01:05:37 -0500

    build(vite): add dynamically generated path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
 import path from "path"
 import { defineConfig } from "vite"
+import { readdirSync } from "fs"
 import react from "@vitejs/plugin-react"
 import checker from "vite-plugin-checker"
 import tsconfigPaths from "vite-tsconfig-paths"
+
+const absolutePathAliases: { [key: string]: string } = {}
+// Root resources folder
+const srcPath = path.resolve("./src/")
+// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
+const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map(
+  (dirent) => dirent.name.replace(/(\.ts){1}(x?)/, ""),
+)
+
+srcRootContent.forEach((directory) => {
+  absolutePathAliases[directory] = path.join(srcPath, directory)
+})
+
+console.log(absolutePathAliases)
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -14,7 +29,7 @@ export default defineConfig(({ mode }) => {
     ],
     resolve: {
       alias: {
-        "~": path.resolve(__dirname, "src"),
+        ...absolutePathAliases,
       },
     },
     server: {
