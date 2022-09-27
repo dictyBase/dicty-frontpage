71bea386924d785ddbf842f1dcb3aaba89c10d35
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-27 00:00:40 -0500

    build(jest config): remove jest.config.js

--- src/assets.d.ts
+++ src/assets.d.ts
@@ -1,8 +1,2 @@
 declare module "*.png"
 declare module "*.jpg"
-
-declare interface clientConfig {
-  google: { clientId: string }
-  linkedin: { clientId: string }
-  orcid: { clientId: string }
-}
