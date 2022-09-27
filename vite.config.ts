2c1ccf85d75d707e118cf579e0960e15ce999d1f
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-26 23:25:05 -0500

    build(vite): remove unnecessary resolve alias config

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,6 @@ import react from "@vitejs/plugin-react"
 import checker from "vite-plugin-checker"
 import tsconfigPaths from "vite-tsconfig-paths"
 
-const absolutePathAliases: { [key: string]: string } = {}
-// Root resources folder
-const srcPath = path.resolve("./src/")
-// Ajust the regex here to include .vue, .js, .jsx, etc.. files from the resources/ folder
-const srcRootContent = readdirSync(srcPath, { withFileTypes: true }).map(
-  (dirent) => dirent.name.replace(/(\.ts){1}(x?)/, ""),
-)
-
-srcRootContent.forEach((directory) => {
-  absolutePathAliases[directory] = path.join(srcPath, directory)
-})
-
-console.log(absolutePathAliases)
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
@@ -27,11 +13,6 @@ export default defineConfig(({ mode }) => {
       react(),
       checker({ overlay: false, typescript: true }),
     ],
-    resolve: {
-      alias: {
-        ...absolutePathAliases,
-      },
-    },
     server: {
       port: 3000,
     },
