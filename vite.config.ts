6386c032db9a634fa4d8f7fb285cc7e32652800c
Siddhartha Basu <biosidd@gmail.com>
2022-09-30 08:08:53 -0500

    ci: remove extra vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
-import path from "path"
 import { defineConfig } from "vite"
-import { readdirSync } from "fs"
 import react from "@vitejs/plugin-react"
-import checker from "vite-plugin-checker"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [
-      tsconfigPaths(),
-      react(),
-      checker({ overlay: false, typescript: true }),
-    ],
-    server: {
-      port: 3000,
-    },
-    build: {
-      outDir: "build",
-    },
-  }
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "build",
+  },
 })
