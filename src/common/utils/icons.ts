7eb5e1c4570dba914752119ad32f64408b2d300a
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-10-04 15:07:23 -0500

    fix(icons.ts): change export default syntax and ignore new error

--- src/common/utils/icons.ts
+++ src/common/utils/icons.ts
@@ -82,5 +82,5 @@ library.add(
   faWrench,
 )
 
-// eslint-disable-next-line no-restricted-exports
-export { library as default } from "@fortawesome/fontawesome-svg-core"
+// eslint-disable-next-line unicorn/prefer-export-from
+export default library
