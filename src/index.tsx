6eee18fc5afc1472a531ff9748d4e113ac4e16c8
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-10-04 02:00:26 -0500

    fix: change default export of App to named export

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import "common/utils/polyfills"
-import React from "react"
 import ReactDOM from "react-dom"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { AuthProvider } from "features/Authentication/AuthStore"
-import App from "app/layout/App"
+import { App } from "app/layout/App"
 import AppProviders from "app/layout/AppProviders"
 import "common/utils/icons" // fontawesome library
 import "fontsource-roboto"
-import worker from "./mocks/browser"
+import worker from "mocks/browser"
 
 const main = async () => {
   // Activate MSW
