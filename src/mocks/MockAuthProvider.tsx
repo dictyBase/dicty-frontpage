2616a3d4937220109c38a611a58389c82d0554d7
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-10-06 00:02:46 -0500

    fix(AuthProvider): memoize state and dispatch object

--- src/mocks/MockAuthProvider.tsx
+++ src/mocks/MockAuthProvider.tsx
@@ -56,7 +56,7 @@ const expiredToken =
 const activeToken =
   "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMiwiZXhwIjo1NTE2MjM5MDIyfQ.Twt1dSBv6Jha3dqWvyUWI4G_ySJsWTD3av30TDtsnyIBPgXwVM3KtPA_YaDw-iO9pfFWZXc2wFUQ6q5WjNwy14yf7IEf2-r_r78jn9tq8a-vSmlr3ieK-Wjg6Y_U8pa4ZXy2zdrtf7IxA2Jz25Vj-BKtW7z_D00qm6EqSGT46fs9Dh0e1zcuCfOwq-STMLFzIbdcpOzvgtyVfyo-P89qhBWooTBt11xR0HeEr5_gJMThXBLtgzT6t_FYzQj3GadPvUQg3gf3qsPOCYk5TNlTIzJXD6yNtncF1MGSpacKTXJFTi3zf_zzpFkBmftPPEicqJo0CrqGO66JdJby8RZE1w"
 
-const MockAuthProvider: React.FC<AuthProperties> = ({
+const MockAuthProvider = ({
   children,
   mocks,
   user = MockSuperuser,
@@ -69,8 +69,13 @@ const MockAuthProvider: React.FC<AuthProperties> = ({
     isAuthenticated: true,
     error: undefined,
   })
+
+  const contextValue = React.useMemo(
+    () => ({ state, dispatch }),
+    [state, dispatch],
+  )
   return (
-    <AuthContext.Provider value={{ state, dispatch }}>
+    <AuthContext.Provider value={contextValue}>
       <MockedProvider mocks={mocks} addTypename={false}>
         <BrowserRouter>{children}</BrowserRouter>
       </MockedProvider>
