2ea1e23a2c2e20f7f387d494711b861d972aeac9
Kevin Tun <kevin.tun@northwestern.edu>
2022-10-11 01:16:01 -0500

    fix(ErrorBoundary.tsx): move state to constructor, disable no-unused-vars
    move state to constructor, disable no-unused-vars eslint error for
    getDerivedStateFromError

--- src/common/components/errors/ErrorBoundary.tsx
+++ src/common/components/errors/ErrorBoundary.tsx
@@ -43,8 +43,12 @@ type State = {
  */
 
 class ErrorBoundary extends Component<Properties, State> {
-  state = { hasError: false }
+  constructor(properties: Properties) {
+    super(properties)
+    this.state = { hasError: false }
+  }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
