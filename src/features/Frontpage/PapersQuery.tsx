4a3e4d181d54dbd9d7727729d7904eba20371236
Kevin Tun <kevin.tun@northwestern.edu>
2022-10-13 19:21:57 -0500

    fix(PapersQuery.tsx): restore accidentally removed conditional return

--- src/features/Frontpage/PapersQuery.tsx
+++ src/features/Frontpage/PapersQuery.tsx
@@ -3,6 +3,7 @@ import GraphQLErrorPage from "common/components/errors/GraphQLErrorPage"
 import Loader from "common/components/Loader"
 import Fallback from "common/components/Fallback"
 import AnnotationsItem from "./AnnotationsItem"
+import PapersItem from "./PapersItem"
 
 interface PapersQueryProperties {
   parent: String
@@ -17,6 +18,7 @@ const PapersQuery = ({ parent }: PapersQueryProperties) => {
 
   if (loading) return <Loader />
   if (error) return <GraphQLErrorPage error={error} />
+  if (parent === "Papers" && data) return <PapersItem data={data} />
   if (parent === "Annotations" && data)
     return <AnnotationsItem data={data} type="publications" />
   return <Fallback />
