50b522f22d7b76b118640b8d0bb45f072ffce991
Kevin Tun <kevin.tun@northwestern.edu>
2022-10-20 11:09:02 -0500

    feat(mockContentBySlug.ts): create mock data for mockContentBySlug

---
+++ src/common/data/mockContentBySlug.ts
@@ -0,0 +1,14 @@
+import MockSuperuser from "./superuser"
+
+const contentBySlug = {
+  content: "content",
+  name: "Content By Slug",
+  // eslint-disable-next-line camelcase
+  updated_at: "1995-09-26",
+  // eslint-disable-next-line camelcase
+  updated_by: MockSuperuser,
+  id: "1",
+  slug: "foo/bar",
+}
+
+export default contentBySlug
